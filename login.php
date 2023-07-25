79362e1561a2876d5bffbe8d6c99a4c0de37c27c
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-25 17:28:00 -0400

    a

--- login.php
+++ login.php
@@ -58,10 +58,10 @@
                   <h3>Incorrect Username/password.</h3><br/>
                   <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
                   </div>";
-        }
-    } else {
-?>
-      <section class="masthead bg-primary text-white text-center">
+           }
+         } else {
+      ?>
+      <header class="masthead bg-primary text-white text-center">
         <form class="form" method="post" name="login">
           <h1 class="login-title">Login</h1>
           <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
@@ -69,7 +69,7 @@
           <input type="submit" value="Login" name="submit" class="login-button"/>
           <p class="link"><a href="registration.php">New Registration</a></p>
         </form>
-      </section>
+      </header>
 <?php
     }
 ?>
