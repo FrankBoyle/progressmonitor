2225d4bf242f0763632f4d846c25ca19c948dffe
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-25 16:51:18 -0400

    a

--- registration.php
+++ registration.php
@@ -64,14 +64,18 @@
         }
     } else {
 ?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="login.php">Click to Login</a></p>
-    </form>
+    <header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+        <form class="form" action="" method="post">
+            <h1 class="login-title">Registration</h1>
+            <input type="text" class="login-input" name="username" placeholder="Username" required />
+            <input type="text" class="login-input" name="email" placeholder="Email Adress">
+            <input type="password" class="login-input" name="password" placeholder="Password">
+            <input type="submit" name="submit" value="Register" class="login-button">
+            <p class="link"><a href="login.php">Click to Login</a></p>
+        </form>
+    </div>
+    </header>
 <?php
     }
 ?>
