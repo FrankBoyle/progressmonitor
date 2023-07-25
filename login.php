2787078a02117bb3d28d52a2f18a4b5d473051d7
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-25 16:49:50 -0400

    a

--- login.php
+++ login.php
@@ -35,25 +35,25 @@
                 </div>
             </div>
         </nav>
-<?php
-    require('db.php');
-    session_start();
-    // When form submitted, check and create user session.
-    if (isset($_POST['username'])) {
-        $username = stripslashes($_REQUEST['username']);    // removes backslashes
-        $username = mysqli_real_escape_string($con, $username);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        // Check user is exist in the database
-        $query    = "SELECT * FROM `accounts` WHERE username='$username'
+      <?php
+        require('db.php');
+        session_start();
+        // When form submitted, check and create user session.
+        if (isset($_POST['username'])) {
+          $username = stripslashes($_REQUEST['username']);    // removes backslashes
+          $username = mysqli_real_escape_string($con, $username);
+          $password = stripslashes($_REQUEST['password']);
+          $password = mysqli_real_escape_string($con, $password);
+          // Check user is exist in the database
+          $query    = "SELECT * FROM `accounts` WHERE username='$username'
                      AND password='" . md5($password) . "'";
-        $result = mysqli_query($con, $query) or die(mysql_error());
-        $rows = mysqli_num_rows($result);
-        if ($rows == 1) {
+          $result = mysqli_query($con, $query) or die(mysql_error());
+          $rows = mysqli_num_rows($result);
+          if ($rows == 1) {
             $_SESSION['username'] = $username;
             // Redirect to user dashboard page
             header("Location: index.php");
-        } else {
+          } else {
             echo "<div class='form'>
                   <h3>Incorrect Username/password.</h3><br/>
                   <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
@@ -61,13 +61,17 @@
         }
     } else {
 ?>
-    <form class="form" method="post" name="login">
-        <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
-        <input type="submit" value="Login" name="submit" class="login-button"/>
-        <p class="link"><a href="registration.php">New Registration</a></p>
-  </form>
+      <header class="masthead bg-primary text-white text-center">
+      <div class="container d-flex align-items-center flex-column">
+        <form class="form" method="post" name="login">
+          <h1 class="login-title">Login</h1>
+          <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+          <input type="password" class="login-input" name="password" placeholder="Password"/>
+          <input type="submit" value="Login" name="submit" class="login-button"/>
+          <p class="link"><a href="registration.php">New Registration</a></p>
+        </form>
+      </div>
+      </header>
 <?php
     }
 ?>
