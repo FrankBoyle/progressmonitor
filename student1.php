aa715856c7628b83814b8d1e5358de33f739686c
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-29 19:18:55 -0400

    a

--- student1.php
+++ student1.php
@@ -15,6 +15,7 @@ include("auth_session.php");
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
