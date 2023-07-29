e9c9808d705a527ea04c1acbad036a49007061fd
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-29 19:52:18 -0400

    a

--- get_table.php
+++ get_table.php
@@ -1,17 +1,9 @@
 <?php
-// Database connection
-$servername = "localhost"; // Replace with your MySQL server name
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
-$dbname = "StudentData"; // Replace with your MySQL database name
+require('db.php');
+include("auth_session.php");
+?>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+<?php
 
 // Retrieve the JSON data from the database
 $sql = "SELECT json_data FROM student1_data ORDER BY id DESC LIMIT 1";
