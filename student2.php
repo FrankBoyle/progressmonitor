2ddbed5f28a01883b8423422b073a8cca4a519ce
FrankBoyle <francisjboyleiii@gmail.com>
2023-07-29 21:18:12 -0400

    a

---
+++ student2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>gilgeekify</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body class="animation">
+    <div id="notification">The Cell Has Been Updated Successfully</div>
+    <h1>Live Table Editor</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Baseline</th>
+                <th>Score1</th>
+                <th>Score2</th>
+                <th>Score3</th>
+                <th>Score4</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider"></tbody>
+    </table>
+    <script src="live-editor.js"></script>
+</body>
+
+</html>
