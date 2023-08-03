cfc4f1a39faedf1e3c4d59c96cea2f51a2a91619
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-03 15:37:45 -0400

    Update login.php

--- login.php
+++ login.php
@@ -964,6 +964,12 @@
   <!-- /.register-box -->
 </section>
   <!-- /.content-wrapper -->
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
   <footer class="main-footer">
     <strong>Copyright &copy; 2023 <a href="https://bfactor.org">Bfactor.org</a>.</strong>
     All rights reserved.
@@ -972,12 +978,7 @@
     </div>
   </footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
+
 
 
 <!-- jQuery -->
