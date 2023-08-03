9f27b7323556d175c8dfc4ec233d9c0cacd248be
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-03 16:41:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -855,6 +855,9 @@
   </aside>
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
 
           </div><!-- /.col -->
         </div><!-- /.row -->
