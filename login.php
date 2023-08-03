55c547183ab850c87316025532daa0575eabd8e5
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-03 17:08:37 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,30 +1,3 @@
-<?php
-    require('./db/db.php');
-    session_start();
-    // When form submitted, check and create user session.
-    if (isset($_POST['email'])) {
-        $email = stripslashes($_REQUEST['email']);    // removes backslashes
-        $email = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        // Check user is exist in the database
-        $query    = "SELECT * FROM `accounts` WHERE email='$email'
-                     AND password='" . md5($password) . "'";
-        $result = mysqli_query($con, $query) or die(mysql_error());
-        $rows = mysqli_num_rows($result);
-        if ($rows == 1) {
-            $_SESSION['email'] = $email;
-            // Redirect to user dashboard page
-            header("Location: index.php");
-             } else {
-            echo "<div class='form'>
-                  <h3>Incorrect email/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                  </div>";
-        }
-    } else {
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -900,6 +873,32 @@
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
+    <?php
+    require('./db/db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['email'])) {
+        $email = stripslashes($_REQUEST['email']);    // removes backslashes
+        $email = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `accounts` WHERE email='$email'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['email'] = $email;
+            // Redirect to user dashboard page
+            header("Location: index.php");
+             } else {
+            echo "<div class='form'>
+                  <h3>Incorrect email/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
       <p class="login-box-msg">Sign in</p>
 
       <form method="post" name="login">
