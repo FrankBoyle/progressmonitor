ad2b29f7dd84c292c97f6b5d051ee0272fa4c2cf
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-08-03 20:41:47 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,34 +1,4 @@
-<?php
-    require('db.php');
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['username'])) {
-        // removes backslashes
-        $fname = stripslashes($_REQUEST['fname']);
-        //escapes special characters in a string
-        $fname = mysqli_real_escape_string($con, $fname);
-        $lname = stripslashes($_REQUEST['lname']);
-        $lname = mysqli_real_escape_string($con, $lname);
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        $create_datetime = date("m-d-Y H:i:s");
-        $query    = "INSERT into `accounts` (fname, lname, email, password, create_datetime)
-                     VALUES ('$fname', '$lname', '$email', '" . md5($password) . "', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
-        if ($result) {
-            echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
-                  </div>";
-        } else {
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='register.php'>register</a> again.</p>
-                  </div>";
-        }
-    } 
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -911,6 +881,37 @@
     <div class="card">
       <div class="card-body register-card-body">
         <p class="login-box-msg">Register a new membership</p>
+        <?php
+    require('db.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])) {
+        // removes backslashes
+        $fname = stripslashes($_REQUEST['fname']);
+        //escapes special characters in a string
+        $fname = mysqli_real_escape_string($con, $fname);
+        $lname = stripslashes($_REQUEST['lname']);
+        $lname = mysqli_real_escape_string($con, $lname);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $create_datetime = date("m-d-Y H:i:s");
+        $query    = "INSERT into `accounts` (fname, lname, email, password, create_datetime)
+                     VALUES ('$fname', '$lname', '$email', '" . md5($password) . "', '$create_datetime')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='register.php'>register</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
         <form method="post" action="" name="registration">
           <div class="input-group mb-3">
             <input type="text" class="form-control" name="fname" id="fname" placeholder="First Name">
@@ -970,6 +971,9 @@
             <!-- /.col -->
           </div>
         </form>
+        <?php
+    }
+?>
         <div class="social-auth-links text-center">
 
           <a href="#" class="btn btn-block btn-danger">
