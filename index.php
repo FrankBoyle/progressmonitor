3cc44a694739fb22de90e8e9ebdb63922ee4f0c6
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-08-03 20:05:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -63,14 +63,8 @@
               </a>
             </div>
       </li>
-      <li class="nav-item dropdown d-none d-sm-inline-block">
-      <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fas fa-sign-out"></i></a>
-            <div class="dropdown-menu">
-              <a href="logout.php" class="dropdown-item">
-                <span href="#" class="dropdown-item">Log Out</button>
-                </a>
-
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="lougout.php" role="button"><i class="fa fa-sign-out"></i></a>
       </li>
     </ul>
 
