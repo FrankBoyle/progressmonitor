ca7f07f6a60aaf29b1c3cd404a91fbeb1d37e17e
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-03 22:22:43 -0400

    a

--- index.php
+++ index.php
@@ -869,8 +869,8 @@ include("./users/auth_session.php");
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+            <li class="breadcrumb-item"><a href="logout.php">Sign Out</a></li>
+
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
