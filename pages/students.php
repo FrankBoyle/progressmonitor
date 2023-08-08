c9c5ac3b4be2909991bd130372a9495131b7b783
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-07 22:02:18 -0400

    Update students.php

--- pages/students.php
+++ pages/students.php
@@ -934,14 +934,14 @@ $conn->close();
                   echo "<td><input type='number' name='score[]' value='{$row["score"]}'></td>";
                   echo "<td><input type='number' name='baseline[]' value='{$row["baseline"]}'></td>";
                   echo "</tr>";
+                  echo "<tr><td colspan="4"><input type='submit' value='Update'></td></tr>";
                 }
               ?>
-              <tr><td colspan="4"><input type='submit' value='Update'></td></tr>
             </table>
           </form>
         <?php else: ?>
         <p>No data available.</p>
-    <?php endif; ?>
+        <?php endif; ?>
 
 </div>
       <!-- /.card -->
