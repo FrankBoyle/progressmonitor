33f65289a8b67cbcd0361714951f9940c557586a
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-07 22:30:24 -0400

    Update students.php

--- pages/students.php
+++ pages/students.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "YourUsername";
-$password = "YourPassword";
-$dbname = "YourDatabaseName";
+$username = "AndersonSchool";
+$password = "SpecialEd69$";
+$dbname = "AndersonSchool";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
