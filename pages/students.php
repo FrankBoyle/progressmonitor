3b2d0fd3ef308ea7a5a32bf9144fbb1e35933ba5
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-07 22:04:15 -0400

    Update students.php

--- pages/students.php
+++ pages/students.php
@@ -934,9 +934,9 @@ $conn->close();
                   echo "<td><input type='number' name='score[]' value='{$row["score"]}'></td>";
                   echo "<td><input type='number' name='baseline[]' value='{$row["baseline"]}'></td>";
                   echo "</tr>";
-                  echo "<td colspan="4"><input type='submit' value='Update'></td>";
                 }
               ?>
+            <tr><td colspan="4"><input type='submit' value='Update'></td></tr>
             </table>
           </form>
         <?php else: ?>
