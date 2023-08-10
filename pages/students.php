69423b8226881a531bdda2f86586ff53d2a0a16f
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-09 22:36:37 -0400

    Update students.php

--- pages/students.php
+++ pages/students.php
@@ -90,7 +90,7 @@ $result = $conn->query($sql);
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist\img\AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="../dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
