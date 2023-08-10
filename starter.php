1214f0397c4714843197cbcdbf986ac5f9504afd
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-10 12:50:54 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -521,12 +521,14 @@ https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css
         }
     };
 
+    const baselineYValue = 5; // Set the baseline y-value here
+
     const baselineSeries = {
         name: 'Baseline',
         type: 'line',
         data: chartData.map(item => ({
             x: new Date(item.x).getTime(),
-            y: item.y2  // Set the baseline y-value here
+            y: baselineYValue
         })),
         // Customizing the line series
         strokeDashArray: 3,
@@ -541,16 +543,13 @@ https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css
         xaxis: {
             type: 'datetime'
         },
-        yaxis: {
-            min: -10,  // Adjust this value based on your data range
-            max: 10    // Adjust this value based on your data range
-        },
         series: [scatterSeries, baselineSeries]
     };
 
     const chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
 </script>
+
 
 
 
