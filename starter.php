09d8cf7176c46ece9ceb064bff9abd63b86f9775
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-09 23:51:11 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -57,8 +57,8 @@ $result = $conn->query($sql);
 
 // Fetch and store data from the database
 $dataArray = array();
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+if ($result1->num_rows > 0) {
+    while ($row = $result1->fetch_assoc()) {
         $dataArray[] = array(
             'x' => $row['date'],     // Use the 'date' column as the x-variable
             'y1' => $row['score'],   // Use the 'score' column as the first y-variable
