d71168c2f8d2e5740aca696eeb7520b0eabba1af
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-08-11 00:57:21 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -556,7 +556,7 @@ if ($chartResult3->num_rows > 0) {
                           shape: "circle",
                           radius: 2,
                           offsetX: 0,
-                          offsetY: 0,
+                          offsetY: 5,
                           onClick: undefined,
                           onDblClick: undefined,
                           showNullDataPoints: true,
