b8a899a27392274e045e75fed79374bdc48cf692
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-08-10 22:05:33 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -133,11 +133,7 @@ if ($chartResult3->num_rows > 0) {
   <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
   
   <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-<link href="
-https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css
-" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+<link href="https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css" rel="stylesheet">
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
