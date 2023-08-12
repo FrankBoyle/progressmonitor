663d1dd7614dfcae95013907435e268d4cd7ee54
FrankBoyle <francisjboyleiii@gmail.com>
2023-08-11 21:41:54 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -550,7 +550,9 @@ var options = {
         {
             name: 'Baseline',
             data: chartData.map(item => ({ x: item.x, y: item.y1 })),
-
+            stroke: {
+              width: 2
+            }
         },
         {
             name: 'Score',
