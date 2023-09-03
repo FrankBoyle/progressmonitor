a60ff7d93e86c851768e9a3a677a81695e3491dc
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-03 12:11:27 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -107,11 +107,9 @@ if ($chartResult3->num_rows > 0) {
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- jsGrid -->
   <link rel="stylesheet" href="../plugins/jsgrid/jsgrid.min.css">
   <link rel="stylesheet" href="../plugins/jsgrid/jsgrid-theme.min.css">
