17de3329ccf963bf6e2a3c44e704080da58b9a4e
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-10 17:06:40 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -660,42 +660,13 @@ chart.render();
             <!-- /.card -->
 
 
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <!--<a href="#" class="card-link">Card link</a>-->
+                <!--<a href="#" class="card-link">Another link</a>-->
               </div>
             </div><!-- /.card -->
           </div>
           <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
 
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-                
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
