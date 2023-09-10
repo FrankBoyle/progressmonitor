bb5b6e308a33b696e75752ae73d5c767d8477b57
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-10 17:10:49 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -367,7 +367,7 @@ if ($chartResult3->num_rows > 0) {
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
-
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
