d70eec5c0b37c83f1bc36202fd8cb303895827b9
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-16 22:36:04 -0400

    Update view_student_data.php

--- view_student_data.php
+++ view_student_data.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $studentId = $_GET['student_id'];
 
@@ -22,6 +24,7 @@ foreach ($performanceData as $data) {
 	echo "<td>" . $data['score8'] . "</td>";
 	echo "<td>" . $data['score9'] . "</td>";
 	echo "<td>" . $data['score10'] . "</td>";
+    // ... continue for other scores
     echo "</tr>";
 }
 
