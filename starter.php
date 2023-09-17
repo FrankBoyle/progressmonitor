321f60153f220ae8ba9748dc366461c81f84b678
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-16 20:27:11 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -690,10 +690,10 @@ chart.render();
 
 <!-- jQuery -->
 
-<script src="./plugins/jquery/jquery.min.js"></script>
+<script type="module" src="./plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="./dist/js/adminlte.min.js"></script>
+<script type="module" src="./dist/js/adminlte.min.js"></script>
 </body>
 </html>
