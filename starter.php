672abcb7e5b781eb776153be457b5281eb1961f4
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-16 21:22:02 -0400

    Update starter.php

--- starter.php
+++ starter.php
@@ -453,12 +453,20 @@ echo "<table border='1'>";
 echo "<tr><th>Week Start Date</th><th>Score1</th><th>Score2</th>...<th>Score10</th></tr>";  // Add more headers if needed
 
 foreach ($performanceData as $data) {
-    echo "<tr>";
-    echo "<td>" . $data['week_start_date'] . "</td>";
-    echo "<td>" . $data['score1'] . "</td>";
-    echo "<td>" . $data['score2'] . "</td>";
-    // ... continue for other scores
-    echo "</tr>";
+  echo "<tr>";
+  echo "<td>" . $data['week_start_date'] . "</td>";
+  echo "<td>" . $data['score1'] . "</td>";
+  echo "<td>" . $data['score2'] . "</td>";
+echo "<td>" . $data['score3'] . "</td>";
+echo "<td>" . $data['score4'] . "</td>";
+echo "<td>" . $data['score5'] . "</td>";
+echo "<td>" . $data['score6'] . "</td>";
+echo "<td>" . $data['score7'] . "</td>";
+echo "<td>" . $data['score8'] . "</td>";
+echo "<td>" . $data['score9'] . "</td>";
+echo "<td>" . $data['score10'] . "</td>";
+  // ... continue for other scores
+  echo "</tr>";
 }
 
 echo "</table>";
