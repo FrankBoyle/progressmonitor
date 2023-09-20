5df1a43dd49e1163326eff4da79a1a724bc78aef
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-20 19:55:47 -0400

    Update update_performance.php

--- update_performance.php
+++ update_performance.php
@@ -8,8 +8,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully to the database";
 }
 
 if ($_POST['performance_id'] && $_POST['field_name'] && $_POST['new_value']) {
@@ -22,17 +20,16 @@ if ($_POST['performance_id'] && $_POST['field_name'] && $_POST['new_value']) {
     $stmt->bind_param('si', $newValue, $performanceId);
     $stmt->execute();
 
-    if ($stmt->affected_rows < 1 || $stmt->error) {
-        $response = array("success" => false, "error" => $stmt->error);
+    if ($stmt->affected_rows < 0 || $stmt->error) {
+        $response = array("success" => false, "error" => "Database error: " . $stmt->error);
     } else {
         $response = array("success" => true);
     }
-    echo json_encode($response);
-    
 
-    // Handle any errors or success messages here
-    // You can return a JSON response indicating success or failure
-    $response = array("success" => true); // Modify as needed
+    echo json_encode($response);
+} else {
+    $response = array("success" => false, "error" => "Invalid data provided.");
     echo json_encode($response);
 }
 ?>
+
