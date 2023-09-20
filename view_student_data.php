bd17e7bb3ee26078213f2249ad240fe7ee273c83
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-20 19:51:09 -0400

    Update view_student_data.php

--- view_student_data.php
+++ view_student_data.php
@@ -1,15 +1,17 @@
 <?php
-    include('./users/db.php');
+// Include database connection and error reporting settings
+include('./users/db.php');
 
-// 1. Error Reporting
+// Error Reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// 2. Check if `student_id` is Set
+// Check if `student_id` is provided in the URL
 if (!isset($_GET['student_id'])) {
     die('Student ID is not provided.');
 }
+
 $studentId = $_GET['student_id'];
 
 $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY week_start_date DESC LIMIT 41");
@@ -17,31 +19,117 @@ $stmt->execute([$studentId]);
 
 $performanceData = $stmt->fetchAll();
 
-// 3. Check Data Retrieval
+// Check Data Retrieval
 if (empty($performanceData)) {
     die('No performance data found for the given student ID.');
 }
 
-echo "<table border='1'>";
-// 4. Handle the `...` in Table Header: For now, I'll just remove the ... for clarity
-echo "<tr><th>Week Start Date</th><th>Score1</th><th>Score2</th><th>Score3</th><th>Score4</th><th>Score5</th><th>Score6</th><th>Score7</th><th>Score8</th><th>Score9</th><th>Score10</th></tr>";
-
-foreach ($performanceData as $data) {
-    echo "<tr>";
-    echo "<td>" . $data['week_start_date'] . "</td>";
-    echo "<td>" . $data['score1'] . "</td>";
-    echo "<td>" . $data['score2'] . "</td>";
-    echo "<td>" . $data['score3'] . "</td>";
-    echo "<td>" . $data['score4'] . "</td>";
-    echo "<td>" . $data['score5'] . "</td>";
-    echo "<td>" . $data['score6'] . "</td>";
-    echo "<td>" . $data['score7'] . "</td>";
-    echo "<td>" . $data['score8'] . "</td>";
-    echo "<td>" . $data['score9'] . "</td>";
-    echo "<td>" . $data['score10'] . "</td>";
-    echo "</tr>";
-}
-
-echo "</table>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Performance Data</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+
+<script>
+$(document).ready(function() {
+    // Add click event handler to editable cells
+    $('.editable').click(function() {
+        const cell = $(this);
+        const originalValue = cell.data('value');
+
+        // Create an input field for editing
+        const input = $('<input type="text">');
+        input.val(originalValue);
+
+        // Replace the cell content with the input field
+        cell.html(input);
+
+        // Focus on the input field
+        input.focus();
+
+        // Add blur event handler to save changes
+        input.blur(function() {
+            const newValue = input.val();
+            cell.data('value', newValue);
+
+            // Update the cell content with the new value
+            cell.text(newValue);
+
+            // Perform AJAX request to update the database with the new value
+            const performanceId = cell.closest('tr').data('performance-id');
+            const fieldName = cell.data('field-name');
+
+            $.ajax({
+                type: 'POST',
+                url: 'update_performance.php',
+                data: {
+                    performance_id: performanceId,
+                    field_name: fieldName,
+                    new_value: newValue,
+                },
+                success: function(response) {
+                    if (response.success) {
+                        alert('Data updated successfully.');
+                    } else {
+                        alert('Error updating data: ' + response.error);
+                    }
+                },
+                error: function() {
+                    alert('Error updating data. Please try again later.');
+                },
+            });
+        });
+
+        // Pressing Enter key while editing should save changes
+        input.keypress(function(e) {
+            if (e.which === 13) {
+                input.blur();
+            }
+        });
+    });
+});
+</script>
+
+<h1>Student Performance Data</h1>
+
+<table border="1">
+    <tr>
+        <th>Week Start Date</th>
+        <th class="editable" data-field-name="score1">Score1</th>
+        <th class="editable" data-field-name="score2">Score2</th>
+        <th class="editable" data-field-name="score3">Score3</th>
+        <th class="editable" data-field-name="score4">Score4</th>
+        <th class="editable" data-field-name="score5">Score5</th>
+        <th class="editable" data-field-name="score6">Score6</th>
+        <th class="editable" data-field-name="score7">Score7</th>
+        <th class="editable" data-field-name="score8">Score8</th>
+        <th class="editable" data-field-name="score9">Score9</th>
+        <th class="editable" data-field-name="score10">Score10</th>
+    </tr>
+
+    <?php foreach ($performanceData as $data) : ?>
+        <tr data-performance-id="<?php echo $data['performance_id']; ?>">
+            <td><?php echo $data['week_start_date']; ?></td>
+            <td data-value="<?php echo $data['score1']; ?>"><?php echo $data['score1']; ?></td>
+            <td data-value="<?php echo $data['score2']; ?>"><?php echo $data['score2']; ?></td>
+            <td data-value="<?php echo $data['score3']; ?>"><?php echo $data['score3']; ?></td>
+            <td data-value="<?php echo $data['score4']; ?>"><?php echo $data['score4']; ?></td>
+            <td data-value="<?php echo $data['score5']; ?>"><?php echo $data['score5']; ?></td>
+            <td data-value="<?php echo $data['score6']; ?>"><?php echo $data['score6']; ?></td>
+            <td data-value="<?php echo $data['score7']; ?>"><?php echo $data['score7']; ?></td>
+            <td data-value="<?php echo $data['score8']; ?>"><?php echo $data['score8']; ?></td>
+            <td data-value="<?php echo $data['score9']; ?>"><?php echo $data['score9']; ?></td>
+            <td data-value="<?php echo $data['score10']; ?>"><?php echo $data['score10']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+
+</table>
+
+</body>
+</html>
 
