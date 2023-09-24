7ca373d5189b07604f31742e006d51bc3148b4e6
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-24 15:00:01 -0400

    a

---
+++ users/fetch_data.php
@@ -0,0 +1,51 @@
+<?php
+// Database connection and error reporting settings
+include('./users/db.php');
+
+// Error Reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Initialize an empty array for performanceData
+$performanceData = [];
+
+// Check if `student_id` is provided in the URL
+if (isset($_GET['student_id'])) {
+    $studentId = $_GET['student_id'];
+
+    $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY week_start_date DESC LIMIT 41");
+    $stmt->execute([$studentId]);
+
+    $performanceData = $stmt->fetchAll();
+}
+
+$scoreNames = [];
+for($i = 1; $i <= 10; $i++) {
+    $scoreNames['score'.$i] = 'Score'.$i;  // default names
+}
+
+if (!empty($performanceData)) {
+    $stmt = $connection->prepare("SELECT original_name, custom_name FROM SchoolScoreNames WHERE SchoolID = ?");
+    $stmt->execute([$schoolID]);
+
+    while ($row = $stmt->fetch()) {
+        $scoreNames[$row['original_name']] = $row['custom_name'];
+    }
+}
+
+// Preparing the data for the chart
+$chartDates = [];
+$chartScores = [];
+
+foreach ($performanceData as $record) {
+    $chartDates[] = $record['week_start_date'];
+    
+    $totalScore = 0;
+    for($i = 1; $i <= 10; $i++) {
+        $totalScore += $record['score'.$i];
+    }
+    $avgScore = $totalScore / 10;
+    $chartScores[] = $avgScore;
+}
+?>
--- view_student_data.php
+++ view_student_data.php
@@ -1,65 +1,51 @@
-<?php
-// Include database connection and error reporting settings
-include('./users/db.php');
-
-// Error Reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Initialize an empty array for performanceData
-$performanceData = array();
-
-// Check if `student_id` is provided in the URL
-if (isset($_GET['student_id'])) {
-    $studentId = $_GET['student_id'];
-
-    $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY week_start_date DESC LIMIT 41");
-    $stmt->execute([$studentId]);
-
-    $performanceData = $stmt->fetchAll();
-}
-
-// Preparing the data for the chart
-$chartDates = [];
-$chartScores = [];
-
-foreach ($performanceData as $record) {
-    $chartDates[] = $record['week_start_date'];
-    
-    // Computing average score for simplicity, adjust as needed
-    $totalScore = 0;
-    for($i = 1; $i <= 10; $i++) {
-        $totalScore += $record['score'.$i];
-    }
-    $avgScore = $totalScore / 10;
-    $chartScores[] = $avgScore;
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Student Performance Data</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-
-    <!-- ApexCharts integration -->
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </head>
-
 <body>
+
+<?php include('fetch_data.php'); ?>
 
 <input type="hidden" id="currentStudentId" value="<?php echo htmlspecialchars($studentId); ?>" />
 <input type="hidden" id="currentWeekStartDate" value="" />
 
-<!-- Adding hidden divs for chart data -->
 <div id="chartDates" style="display:none;"><?php echo json_encode($chartDates); ?></div>
 <div id="chartScores" style="display:none;"><?php echo json_encode($chartScores); ?></div>
+
+<h1>Student Performance Data</h1>
+<button id="addDataRow">Add Data Row</button>
+
+<table border="1">
+    <tr>
+        <th class="editable" data-field-name="week_start_date">Week Start Date</th>
+        <!-- Add other headers using loop based on score names-->
+        <?php foreach ($scoreNames as $key => $name): ?>
+            <th class="editable" data-field-name="<?php echo $key; ?>"><?php echo $name; ?></th>
+        <?php endforeach; ?>
+    </tr>
+    <?php if (empty($performanceData)): ?>
+        <tr>
+            <td colspan="11">No Data Found. Click "Add Data Row" to add new data.</td>
+        </tr>
+    <?php else: ?>
+        <?php foreach ($performanceData as $data): ?>
+            <tr data-performance-id="<?php echo $data['performance_id']; ?>">
+                <td class="editable" data-field-name="week_start_date"><?php echo date("m/d/Y", strtotime($data['week_start_date'])); ?></td>
+                <!-- Add scores using loop -->
+                <?php for ($i = 1; $i <= 10; $i++): ?>
+                    <td class="editable" data-field-name="score<?php echo $i; ?>"><?php echo $data['score'.$i]; ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</table>
 
 <script>
 $(document).ready(function() {
@@ -182,97 +168,5 @@ $('#addDataRow').click(function() {
 });
 </script>
 
-
-<h1>Student Performance Data</h1>
-<button id="addDataRow">Add Data Row</button>
-
-<table border="1">
-    <tr>
-        <th class="editable" data-field-name="week_start_date">Week Start Date</th>
-        <th class="editable" data-field-name="score1">Score1</th>
-        <th class="editable" data-field-name="score2">Score2</th>
-        <th class="editable" data-field-name="score3">Score3</th>
-        <th class="editable" data-field-name="score4">Score4</th>
-        <th class="editable" data-field-name="score5">Score5</th>
-        <th class="editable" data-field-name="score6">Score6</th>
-        <th class="editable" data-field-name="score7">Score7</th>
-        <th class="editable" data-field-name="score8">Score8</th>
-        <th class="editable" data-field-name="score9">Score9</th>
-        <th class="editable" data-field-name="score10">Score10</th>
-
-        <!-- more headers here -->
-    </tr>
-    <?php if (empty($performanceData)) : ?>
-        <tr><td colspan="11">No Data Found. Click "Add Data Row" to add new data.</td></tr>
-    <?php else : ?>
-        <?php foreach ($performanceData as $data) : ?>
-            <tr data-performance-id="<?php echo $data['performance_id']; ?>">
-            <td class="editable" data-field-name="week_start_date"><?php echo date("m/d/Y", strtotime($data['week_start_date'])); ?></td>
-                <td class="editable" data-field-name="score1"><?php echo $data['score1']; ?></td>
-                <td class="editable" data-field-name="score2"><?php echo $data['score2']; ?></td>
-                <td class="editable" data-field-name="score3"><?php echo $data['score3']; ?></td>
-                <td class="editable" data-field-name="score4"><?php echo $data['score4']; ?></td>
-                <td class="editable" data-field-name="score5"><?php echo $data['score5']; ?></td>
-                <td class="editable" data-field-name="score6"><?php echo $data['score6']; ?></td>
-                <td class="editable" data-field-name="score7"><?php echo $data['score7']; ?></td>
-                <td class="editable" data-field-name="score8"><?php echo $data['score8']; ?></td>
-                <td class="editable" data-field-name="score9"><?php echo $data['score9']; ?></td>
-                <td class="editable" data-field-name="score10"><?php echo $data['score10']; ?></td>
-
-                <!-- Add other columns here -->
-            </tr>
-
-        <?php endforeach; ?>
-    <?php endif; ?>
-</table>
-<script src="https://quickchart.io/apex-charts/render"></script>
-
-<div id="chart"></div>
-<script>
-// Including ApexCharts
-document.write('<script src="https://cdn.jsdelivr.net/npm/apexcharts"><\/script>');
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Fetch data from PHP and convert to Javascript arrays
-    var chartDates = JSON.parse(document.getElementById('chartDates').textContent);
-    var chartScores = JSON.parse(document.getElementById('chartScores').textContent);
-
-    // Setting up the options for the chart
-    var options = {
-        series: [{
-            name: 'Average Score',
-            data: chartScores
-        }],
-        chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-                enabled: false
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'smooth'
-        },
-        xaxis: {
-            type: 'datetime',
-            categories: chartDates.map(date => new Date(date).getTime())
-        },
-        tooltip: {
-            x: {
-                format: 'dd/MM/yy HH:mm'
-            },
-        },
-    };
-
-    // Render the chart
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-});
-</script>
 </body>
 </html>
-
-
