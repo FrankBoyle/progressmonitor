e39399eb64e6ddb7a072bb09ad4b91df175d775e
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-30 18:04:17 -0400

    Update insert_performance.php

--- insert_performance.php
+++ insert_performance.php
@@ -1,36 +1,48 @@
 <?php
+// Include the database connection script
 include('./users/db.php');
 
+// Function to log errors server-side
+function logError($error) {
+    // Log error to a file (Make sure your server has write permissions for this file)
+    file_put_contents('error_log.txt', $error . PHP_EOL, FILE_APPEND);
+}
+
+$responseData = [];
+
+// Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Simple validations (You can expand upon these based on your needs)
+
+    // Validate that the necessary POST data is present
     if (empty($_POST['student_id']) || empty($_POST['week_start_date']) || empty($_POST['scores'])) {
-        echo json_encode(['error' => 'Required data is missing']);
-        exit;
-    }
-
-    $studentId = $_POST['student_id'];
-    $weekStartDate = $_POST['week_start_date'];
-    $scores = $_POST['scores']; // Assuming scores are passed as an array
-
-    // Insert the new performance data into the database
-    $stmt = $connection->prepare("INSERT INTO Performance (student_id, week_start_date, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    
-    if ($stmt->execute([$studentId, $weekStartDate, $scores['score1'], $scores['score2'], $scores['score3'], $scores['score4'], $scores['score5'], $scores['score6'], $scores['score7'], $scores['score8'], $scores['score9'], $scores['score10']])) {
-        // Optionally return the inserted data to the client
-        $newPerformanceId = $connection->lastInsertId();
-        $responseData = [
-            'performance_id' => $newPerformanceId,
-            'week_start_date' => $weekStartDate,
-            'scores' => $scores,
-        ];
-
-        echo json_encode($responseData);
+        logError("Required data is missing.");
+        $responseData['error'] = 'An error occurred. Please try again later.';
     } else {
-        echo json_encode(['error' => 'Failed to insert data']);
+        $studentId = $_POST['student_id'];
+        $weekStartDate = $_POST['week_start_date'];
+        $scores = $_POST['scores']; // Assuming scores are passed as an array
+
+        $stmt = $connection->prepare("INSERT INTO Performance (student_id, week_start_date, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        if ($stmt->execute([$studentId, $weekStartDate, $scores['score1'], $scores['score2'], $scores['score3'], $scores['score4'], $scores['score5'], $scores['score6'], $scores['score7'], $scores['score8'], $scores['score9'], $scores['score10']])) {
+            $newPerformanceId = $connection->lastInsertId();
+            $responseData = [
+                'performance_id' => $newPerformanceId,
+                'week_start_date' => $weekStartDate,
+                'scores' => $scores,
+            ];
+        } else {
+            logError("Failed to insert data: " . $stmt->error);
+            $responseData['error'] = 'An error occurred. Please try again later.';
+        }
     }
 
 } else {
-    echo 'Invalid request';
+    logError("Invalid request method.");
+    $responseData['error'] = 'Invalid request.';
 }
+
+// Return response data as JSON
+echo json_encode($responseData);
 
 ?>
