eff3f49069b5f7e854abd112c5afc888fd0777e1
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-30 18:11:46 -0400

    Update update_performance.php

--- update_performance.php
+++ update_performance.php
@@ -1,39 +1,65 @@
 <?php
+include('./users/db.php');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$username = "AndersonSchool";
-$password = "SpecialEd69$";
-$dbname = "bFactor-test";
-
+// Create a new database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check the database connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    handleError("Connection failed: " . $conn->connect_error);
 }
 
-if ($_POST['performance_id'] && $_POST['field_name'] && $_POST['new_value']) {
-    $performanceId = $_POST['performance_id'];
-    $fieldName = $_POST['field_name'];
-    $newValue = $_POST['new_value'];
+// Main logic
+if (isset($_POST['performance_id'], $_POST['field_name'], $_POST['new_value'])) {
+    updatePerformance($conn, $_POST['performance_id'], $_POST['field_name'], $_POST['new_value']);
+} else {
+    handleError("Invalid data provided.");
+}
 
-    // Update the database
-    $stmt = $conn->prepare("UPDATE Performance SET $fieldName = ? WHERE performance_id = ?");
-    $stmt->bind_param('si', $newValue, $performanceId);
+/**
+ * Function to update the Performance data.
+ */
+function updatePerformance($conn, $performanceId, $fieldName, $newValue) {
+    // List of allowed field names to ensure security
+    $allowedFields = ['week_start_date', 'score1', 'score2', 'score3', 'score4', 'score5', 'score6', 'score7', 'score8', 'score9', 'score10'];
 
-    if ($stmt->execute()) {
-        $response = array("success" => true);
-    } else {
-        $response = array("success" => false, "error" => "Database error: " . $stmt->error);
+    if (!in_array($fieldName, $allowedFields)) {
+        handleError("Invalid field specified.");
+        return;
     }
 
-    echo json_encode($response);
-} else {
-    $response = array("success" => false, "error" => "Invalid data provided.");
-    echo json_encode($response);
+    // Prepare SQL statement
+    $sql = "UPDATE Performance SET $fieldName = ? WHERE performance_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('si', $newValue, $performanceId);
+
+    // Execute and respond
+    if ($stmt->execute()) {
+        sendResponse(["success" => true]);
+    } else {
+        handleError("Database error: " . $stmt->error);
+    }
 }
+
+/**
+ * Function to handle errors.
+ */
+function handleError($errorMessage) {
+    sendResponse(["success" => false, "error" => $errorMessage]);
+}
+
+/**
+ * Function to send a JSON response.
+ */
+function sendResponse($response) {
+    echo json_encode($response);
+    exit;
+}
+
 ?>
 
 
