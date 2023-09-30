d64a579eaf63e4f67c2780b16bfa2e72e9fbdcd3
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-30 18:30:58 -0400

    a

--- update_performance.php
+++ update_performance.php
@@ -1,6 +1,4 @@
-
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -54,7 +52,6 @@ function sendResponse($response) {
     echo json_encode($response);
     exit;
 }
-
 ?>
 
 
