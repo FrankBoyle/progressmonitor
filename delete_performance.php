cbbae34e47fa825e64ef8bd7fff1b22c3be15ee5
FrankBoyle <francisjboyleiii@gmail.com>
2023-09-30 22:23:04 -0400

    Update delete_performance.php

--- delete_performance.php
+++ delete_performance.php
@@ -3,44 +3,34 @@ file_put_contents('post_log.txt', print_r($_POST, true));
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 include('./users/db.php');
-
-if (!$connection) {
-    die("Connection failed: " . $connection->errorInfo());
-}
-
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $response = ['success' => false, 'message' => 'Unknown error.'];
 
 if (isset($_POST['performance_id'])) {
     $performanceId = $_POST['performance_id'];
 
-    // Ensure you use prepared statements to prevent SQL injection
-    $stmt = $connection->prepare("DELETE FROM Performance WHERE performance_id = ?");
-    if ($stmt === false) {
-        $response['message'] = "Failed to prepare the statement. Error: " . $connection->error;
-    } else {
-        $stmt->bind_param("i", $performanceId);
+    try {
+        // Prepare the DELETE statement
+        $stmt = $connection->prepare("DELETE FROM Performance WHERE performance_id = :performanceId");
+        
+        // Bind the parameters
+        $stmt->bindParam(':performanceId', $performanceId, PDO::PARAM_INT);
+        
+        // Execute the statement
+        $stmt->execute();
 
-        try {
-            $stmt->execute();
-            if ($stmt->rowCount() > 0) {
-                $response['success'] = true;
-            } else {
-                $response['message'] = "No rows affected. The provided performance_id might not exist in the database.";
-            }
-        } catch (PDOException $e) {
-            $response['message'] = "Database error: " . $e->getMessage();
+        if ($stmt->rowCount() > 0) {
+            $response['success'] = true;
+        } else {
+            $response['message'] = "No rows affected. The provided performance_id might not exist in the database.";
         }
-        
-        
-
-        $stmt->close();
+    } catch (PDOException $e) {
+        $response['message'] = "Database error: " . $e->getMessage();
     }
 }
 
 echo json_encode($response);
+
 ?>
 
