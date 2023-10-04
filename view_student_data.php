ba00969de8fab79195bf11136a6928b655bb85c0
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-04 19:09:39 -0400

    Update view_student_data.php

--- view_student_data.php
+++ view_student_data.php
@@ -66,10 +66,11 @@
 
 <label>Select Score to Display: </label>
 <select id="scoreSelector">
-    <?php for ($i = 1; $i <= 10; $i++): ?>
-        <option value="score<?php echo $i; ?>">Score <?php echo $i; ?></option>
-    <?php endfor; ?>
+    <?php foreach ($scoreNames as $original => $custom): ?>
+        <option value="<?php echo $original; ?>"><?php echo $custom; ?></option>
+    <?php endforeach; ?>
 </select>
+
 
 <label>Enter Benchmark Value: </label>
 <input type="text" id="benchmarkValue">
