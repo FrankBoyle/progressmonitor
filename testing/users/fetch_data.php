77bdc3f669119141940643adaef3d8c20f603cb9
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-11 20:37:27 -0400

    testing environment

---
+++ testing/users/fetch_data.php
@@ -0,0 +1,131 @@
+<?php
+include('./users/db.php');
+
+// Error Reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function fetchPerformanceData($studentId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY week_start_date DESC LIMIT 41");
+    $stmt->execute([$studentId]);
+    return $stmt->fetchAll();
+}
+
+function fetchSchoolIdForStudent($studentId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT SchoolID FROM Students WHERE student_id = ?");
+    $stmt->execute([$studentId]);
+    $result = $stmt->fetch();
+    return $result ? $result['SchoolID'] : null;
+}
+
+function fetchScoreNames($schoolID) {
+    global $connection;
+    $scoreNames = [];
+    $stmt = $connection->prepare("SELECT original_name, custom_name FROM SchoolScoreNames WHERE SchoolID = ?");
+    $stmt->execute([$schoolID]);
+    while ($row = $stmt->fetch()) {
+        $scoreNames[$row['original_name']] = $row['custom_name'];
+    }
+    return $scoreNames;
+}
+
+function fetchStudentsByTeacher($teacherId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT s.* FROM Students s INNER JOIN Teachers t ON s.SchoolID = t.SchoolID WHERE t.teacher_id = ?");
+    $stmt->execute([$teacherId]);
+    return $stmt->fetchAll();
+}
+
+function addNewStudent($studentName, $teacherId) {
+    global $connection;
+
+    // Fetch the SchoolID of the current teacher
+    $stmt = $connection->prepare("SELECT SchoolID FROM Teachers WHERE teacher_id = ?");
+    $stmt->execute([$teacherId]);
+    $teacherInfo = $stmt->fetch();
+    $teacherSchoolID = $teacherInfo['SchoolID'];
+
+    // Check if the student with the same name and SchoolID already exists
+    $stmt = $connection->prepare("SELECT student_id FROM Students WHERE name = ? AND SchoolID = ?");
+    $stmt->execute([$studentName, $teacherSchoolID]);
+    $duplicateStudent = $stmt->fetch();
+
+    if ($duplicateStudent) {
+        return "Student with the same name already exists.";
+    } 
+
+    // Insert the new student with the same SchoolID
+    $stmt = $connection->prepare("INSERT INTO Students (name, SchoolID) VALUES (?, ?)");
+    $stmt->execute([$studentName, $teacherSchoolID]);
+    return "New student added successfully.";
+}
+
+function fetchGroupNames() {
+    global $connection;
+    $stmt = $connection->prepare("SELECT group_name FROM ScoreGroups");
+    $stmt->execute();
+    $stmt->bindColumn(1, $groupName);
+    
+    $groups = [];
+    while ($stmt->fetch(PDO::FETCH_BOUND)) {
+        $groups[] = $groupName;
+    }
+    
+    return $groups;
+}
+
+
+// Initialize empty arrays and variables
+$performanceData = [];
+$scoreNames = [];
+$chartDates = [];
+$chartScores = [];
+
+// Check if the action is set to 'fetchGroups' and handle it
+if (isset($_GET['action']) && $_GET['action'] == 'fetchGroups') {
+    echo json_encode(fetchGroupNames());
+    exit;
+}
+
+// If student_id is not set, exit early
+if (!isset($_GET['student_id'])) {
+    return;
+}
+
+$studentId = $_GET['student_id'];
+$schoolID = fetchSchoolIdForStudent($studentId);  // Fetch SchoolID
+
+if (!$schoolID) {
+    return;  // If there's no SchoolID, exit early
+}
+
+// Fetch performance data and score names
+$performanceData = fetchPerformanceData($studentId);
+$scoreNames = fetchScoreNames($schoolID);
+
+// Preparing the data for the chart
+foreach ($performanceData as $record) {
+    $chartDates[] = $record['week_start_date'];
+    // You can add more logic here if needed
+}
+
+// Handling the data POST from the dropdown functionality
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ScoreGroup'])) {
+    $schoolIDIndex = $_POST['SchoolIDIndex'];
+    $originalName = $_POST['original_name'];
+    $customName = $_POST['custom_name'];
+    $scoreGroup = $_POST['ScoreGroup'];
+
+    // Inserting into the SchoolScoreNames table
+    $stmt = $connection->prepare("INSERT INTO SchoolScoreNames (SchoolIDIndex, original_name, custom_name, group_name) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$schoolIDIndex, $originalName, $customName, $scoreGroup]);
+    
+    // Respond with the ID of the inserted row
+    echo json_encode(['id' => $connection->lastInsertId()]);
+    exit;
+}
+?>
+
---
+++ testing/view_student_data.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Performance Data</title>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="student_data.js"></script>
+
+<style>
+    .dataTables_filter {
+        display: none;
+    }
+</style>
+</head>
+<body>
+
+<?php include('./users/fetch_data.php'); ?>
+
+<a href="test.php" class="btn btn-primary">Student List</a>
+
+<h1>Student Performance Data</h1>
+<button id="addDataRow">Add Data Row</button>
+
+<label for="startDateFilter">Filter by Start Date:</label>
+<input type="text" id="startDateFilter">
+
+<label>Select Group to Display: </label>
+<select id="groupSelector">
+    <?php foreach ($groupNames as $groupName): ?>
+        <option value="<?php echo htmlspecialchars($groupName); ?>"><?php echo htmlspecialchars($groupName); ?></option>
+    <?php endforeach; ?>
+</select>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>Week Start Date</th>
+            <?php foreach ($scoreNames as $key => $name): ?>
+                <th><?php echo $name; ?></th>
+            <?php endforeach; ?>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <?php if (empty($performanceData)): ?>
+        <tr>
+            <td colspan="11">No Data Found. Click "Add Data Row" to add new data.</td>
+        </tr>
+    <?php else: ?>
+        <?php foreach ($performanceData as $data): ?>
+            <tr data-performance-id="<?php echo $data['performance_id']; ?>">
+                <td class="editable" data-field-name="week_start_date">
+                    <?php
+                    if (isset($data['week_start_date'])) {
+                        echo date("m/d/Y", strtotime($data['week_start_date']));
+                    }
+                    ?>
+                </td>
+                <!-- Add scores using loop -->
+                <?php for ($i = 1; $i <= 10; $i++): ?>
+                    <td class="editable" data-field-name="score<?php echo $i; ?>">
+                        <?php
+                        if (isset($data['score'.$i])) {
+                            echo $data['score'.$i];
+                        }
+                        ?>
+                    </td>
+                <?php endfor; ?>
+                <td><button class="deleteRow" data-performance-id="<?php echo $data['performance_id']; ?>">Delete</button></td> <!-- New delete button for each row -->
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</table>
+
+<label>Select Score to Display: </label>
+<select id="scoreSelector">
+    <?php foreach ($scoreNames as $key => $name): ?>
+        <option value="<?php echo $key; ?>"><?php echo htmlspecialchars($name); ?></option>
+    <?php endforeach; ?>
+</select>
+
+
+<label>Enter Benchmark Value: </label>
+<input type="text" id="benchmarkValue">
+<button id="updateBenchmark">Update Benchmark</button>
+
+<div id="chart"></div>  <!-- Div to display the chart -->
+
+
+</body>
+</html>
