a0d37576eaddf1ef365a735cf101d7a86f433db3
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-12 20:25:35 -0400

    a

--- users/fetch_data.php
+++ users/fetch_data.php
@@ -1,10 +1,10 @@
 <?php
-include('./users/db.php');
-
 // Error Reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+include('./users/db.php');
 
 function fetchPerformanceData($studentId) {
     global $connection;
