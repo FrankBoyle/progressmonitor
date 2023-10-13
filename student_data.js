18693d62700bb8acdd78053151f06881431e63d2
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-13 11:16:49 -0400

    Update student_data.js

--- student_data.js
+++ student_data.js
@@ -145,7 +145,7 @@ function getChartOptions(dataSeries, xCategories) {
             size: 5,
             colors: undefined,
             strokeColors: '#fff',
-            strokeWidth: 0.5,
+            strokeWidth: 1,
             strokeOpacity: 0,
             strokeDashArray: 0,
             fillOpacity: 1,
