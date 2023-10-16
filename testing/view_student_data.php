dbe3111e55129e48b573f2135c8bfb1693c653be
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-15 22:03:34 -0400

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -93,10 +93,9 @@ foreach ($performanceData as $record) {
 <thead>
     <tr>
         <th><?php echo isset($scoreNames['score_date']) ? $scoreNames['score_date'] : ''; ?></th>
-        <!-- Dynamically generate table headers based on $displayedColumns -->
         <?php foreach ($displayedColumns as $columnName): ?>
             <?php if ($columnName !== "Week Start Date"): ?>
-                <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : ''; ?></th>
+                <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : $columnName; ?></th>
             <?php endif; ?>
         <?php endforeach; ?>
         <th>Action</th>
