8dc7599a27626620f31a446b2cf613dd495ee0f8
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-16 19:32:15 -0400

    Update test.php

--- testing/test.php
+++ testing/test.php
@@ -2,6 +2,10 @@
 include './users/fetch_data.php';
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['teacher_id'])) {
     die("Teacher ID not set in session");
 }
