7a85424f6e2fc0098352f32d6b11f93872769390
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-16 13:35:19 -0400

    Update fetch_data.php

--- testing/users/fetch_data.php
+++ testing/users/fetch_data.php
@@ -15,7 +15,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$teacherId = $_GET['teacher_id'];
+//$teacherId = $_GET['teacher_id'];
 // Continue with the rest of your script...
 
 
