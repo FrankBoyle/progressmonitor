79943cf1dfe0c294f61f37652af270e5cfd42253
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-15 21:50:01 -0400

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -86,19 +86,19 @@ foreach ($performanceData as $record) {
     <?php endforeach; ?>
 </select>
 
-<thead>
-    <tr>
-        <th><?php echo isset($scoreNames['score_date']) ? $scoreNames['score_date'] : ''; ?></th>
-        <!-- Dynamically generate table headers based on $displayedColumns -->
-        <?php foreach ($displayedColumns as $columnName): ?>
-            <?php if ($columnName !== "Week Start Date"): ?>
-            <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : ''; ?></th>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <th>Action</th>
-    </tr>
-</thead>
-
+<table border="1">
+    <thead>
+        <tr>
+            <th><?php echo isset($scoreNames['score_date']) ? $scoreNames['score_date'] : ''; ?></th>
+            <!-- Dynamically generate table headers based on $displayedColumns -->
+            <?php foreach ($displayedColumns as $columnName): ?>
+                <?php if ($columnName !== "Week Start Date"): ?>
+                    <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : ''; ?></th>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <th>Action</th>
+        </tr>
+    </thead>
 
     <?php if (!empty($performanceData)): ?>
         <?php foreach ($performanceData as $data): ?>
