8f066446029242a6498206b73434cef3117aa7f1
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-15 22:10:11 -0400

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -85,9 +85,6 @@ foreach ($performanceData as $record) {
         <option value="<?php echo $entry['metadata_id']; ?>"><?php echo $entry['category_name']; ?></option>
     <?php endforeach; ?>
 </select>
-<?php var_dump($scoreNames); ?>
-<?php var_dump($displayedColumns); ?>
-<?php var_dump($columnName); ?>
 
 <table border="1">
 <thead>
