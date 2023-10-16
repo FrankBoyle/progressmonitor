a98c4d2064b2dbe91ce3a63b86fb1dd42f93e1f1
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-16 13:23:33 -0400

    Update test.php

--- testing/test.php
+++ testing/test.php
@@ -1,33 +1,16 @@
 <?php
-session_start(); // Make sure to start the session at the beginning of each page
+session_start(); // Start the session at the beginning of the page
 include './users/fetch_data.php';
 
-if (isset($_SESSION['teacher_id'])) {
-    $teacherId = $_SESSION['teacher_id'];
-    // Now you can use $teacherId in your page logic
-} else {
-    // Handle the case where teacher_id is not set in the session
-    echo "Teacher ID is missing in the session.";
+if (!isset($_SESSION['teacher_id'])) {
+    header("Location: login.php"); // Redirect to the login page if teacher_id is not set in the session
+    exit;
 }
 
-error_log(print_r($_SESSION, true)); // Log the session variables
-
-if ($teacherResult) {
-    $_SESSION['teacher_id'] = $teacherResult['teacher_id'];
-    // Assuming you have fetched the SchoolID from somewhere in your code
-    $_SESSION['SchoolID'] = $schoolIdFromDatabase;
-
-    // Redirect to the desired page
-    header("Location: test.php");
-    exit();
-} else {
-    echo '<p class="error">Username or password is incorrect!</p>';
-}
+// Now you can safely use $_SESSION['teacher_id'] in your page logic.
+$teacherId = $_SESSION['teacher_id'];
 
 $message = "";  // Initialize an empty message variable
-
-// Define a default metadata ID (you can change this as needed)
-$defaultMetadataID = 1;
 
 // Handle form submission for adding new student
 if (isset($_POST['add_new_student'])) {
@@ -54,20 +37,17 @@ $students = fetchStudentsByTeacher($teacherId);
         <input type="submit" name="add_new_student" value="Add New Student">
     </form>
 
-    <?php if (!empty($students)): ?>
-    <h2>Students:</h2>
-    <?php foreach ($students as $student): ?>
-        <?php
-        // Dynamically generate the link with metadata_id as a query parameter
-        $studentLink = 'view_student_data.php?student_id=' . $student['student_id'] . '&metadata_id=' . $defaultMetadataID;
-        ?>
-        <a href="<?= $studentLink ?>"><?= $student['name'] ?></a><br>
-    <?php endforeach; ?>
-<?php else: ?>
-    No students found for this teacher.
-<?php endif; ?>
+    <?php if ($message): ?>
+        <p><?= $message ?></p>
+    <?php endif; ?>
 
+    <?php if (!empty($students)): ?>
+        <h2>Students:</h2>
+        <?php foreach ($students as $student): ?>
+            <a href='view_student_data.php?student_id=<?= $student['student_id'] ?>'><?= $student['name'] ?></a><br>
+        <?php endforeach; ?>
+    <?php else: ?>
+        No students found for this teacher.
+    <?php endif; ?>
 </body>
 </html>
-
-
