e88df3ced4111388337de23753425da554eba24e
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-16 22:33:36 -0400

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -75,14 +75,15 @@ foreach ($performanceData as $record) {
     <tr>
         <th>Date</th>
         <!-- Dynamically generate table headers based on $displayedColumns -->
-        <?php foreach ($displayedColumns as $columnName => $displayName): ?>
-            <?php if ($columnName !== "Date"): ?>
-                <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : $displayName; ?></th>
+        <?php foreach ($displayedColumns as $columnName => $columnLabel): ?>
+            <?php if ($columnName !== "score_date"): ?>
+                <th><?php echo isset($scoreNames[$columnName]) ? $scoreNames[$columnName] : $columnLabel; ?></th>
             <?php endif; ?>
         <?php endforeach; ?>
         <th>Action</th>
     </tr>
 </thead>
+
 <?php if (!empty($performanceData)): ?>
     <?php foreach ($performanceData as $data): ?>
         <tr data-performance-id="<?php echo $data['performance_id']; ?>">
@@ -91,7 +92,7 @@ foreach ($performanceData as $record) {
             </td>
             <!-- Dynamically generate table cells for scores based on $displayedColumns -->
             <?php foreach ($displayedColumns as $columnName => $columnLabel): ?>
-                <?php if ($columnName !== "Date"): ?>
+                <?php if ($columnName !== "score_date"): ?>
                     <td class="editable" data-field-name="<?php echo $columnName; ?>">
                         <?php echo isset($data[$columnName]) ? $data[$columnName] : ''; ?>
                     </td>
@@ -105,7 +106,9 @@ foreach ($performanceData as $record) {
         <td colspan="<?php echo count($displayedColumns) + 1; ?>">No data available.</td>
     </tr>
 <?php endif; ?>
+
 </table>
+
 
 <label>Select Score to Display: </label>
 <select id="scoreSelector">
