7dbf7272b2037d91258f98ec5bde451e4ab61308
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-16 20:35:16 -0400

    Update test.php

--- testing/test.php
+++ testing/test.php
@@ -24,7 +24,7 @@ if (isset($_POST['add_new_student'])) {
     }
 }
 
-$students = fetchStudentsByTeacher($teacherId);
+$students = fetchStudentsByTeacher($connection, $teacherId);
 ?>
 
 <!DOCTYPE html>
