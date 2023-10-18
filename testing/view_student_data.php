407705e00409ed49aaeba003699bb30154e72abe
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-17 23:25:35 -0400

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -42,7 +42,7 @@ foreach ($performanceData as $record) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="student_data.js"></script>
 
 <style>
