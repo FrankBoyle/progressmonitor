3cca3d1f41ea27b77f38a98cbbf5610d7535fc10
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-18 19:28:15 -0400

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -260,6 +260,7 @@ $(document).ready(function () {
     const CURRENT_STUDENT_ID = $('#currentStudentId').val();
     var metadataId; // Declare metadataId in the outer scope
     var performanceData; // Define the performanceData variable
+    const columnNames = Object.values(columnHeaders);
 
     let dateAscending = true;
     // Initialize the page
