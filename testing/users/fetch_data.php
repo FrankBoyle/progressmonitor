91ca4219c7387c51bd6329d0743e6e059482ada6
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-19 13:45:25 -0400

    Update fetch_data.php

--- testing/users/fetch_data.php
+++ testing/users/fetch_data.php
@@ -85,18 +85,17 @@ function fetchGroupNames() {
     return $groups;
 }
 
-// Function to get the smallest metadata_id for a given school_id
 function getSmallestMetadataId($schoolId) {
-    global $db; // Access the database connection
+    global $connection;
 
     // Prepare and execute a query to fetch the smallest metadata_id
     $query = "SELECT MIN(metadata_id) AS smallest_metadata_id FROM Metadata WHERE school_id = :schoolId";
-    $statement = $db->prepare($query);
-    $statement->bindParam(':schoolId', $schoolId, PDO::PARAM_INT);
-    $statement->execute();
+    $stmt = $connection->prepare($query);
+    $stmt->bindParam(':schoolId', $schoolId, PDO::PARAM_INT);
+    $stmt->execute();
 
     // Fetch the result
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // Check if a result was found
     if ($result && isset($result['smallest_metadata_id'])) {
