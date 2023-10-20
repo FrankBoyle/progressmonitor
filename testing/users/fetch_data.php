fd3c4376552b11f786a9b5952bdf0bef0a8deb78
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-20 07:43:57 -0400

    Update fetch_data.php

--- testing/users/fetch_data.php
+++ testing/users/fetch_data.php
@@ -7,12 +7,6 @@ error_reporting(E_ALL);
 
 include('db.php');
 
-// Example of initializing variables with default values
-$teacherId = null;
-$studentId = null;
-
-// Rest of your code...
-
 function fetchPerformanceData($studentId) {
     global $connection;
     $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY score_date DESC LIMIT 41");
