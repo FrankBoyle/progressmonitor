cb392f9914e3ab6eed6145194e6c5d2c5e5a524c
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-21 00:19:26 -0400

    a

---
+++ testing/view_student_data.php
@@ -0,0 +1,154 @@
+<?php
+include ('./users/fetch_data.php');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//$metadata_id = !empty($_GET['metadata_id']) ? $_GET['metadata_id'] : null;
+//echo '<pre>';
+//print_r($_GET);
+//echo '</pre>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Performance Data</title>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="student_data.js"></script>
+    <script>
+    // Get the metadata_id from the URL parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const metadata_id = urlParams.get('metadata_id');
+    </script>
+
+<style>
+    .dataTables_filter {
+        display: none;
+    }
+</style>
+</head>
+<body>
+<a href="test.php" class="btn btn-primary">Student List</a>
+<input type="hidden" id="schoolIdInput" name="school_id" value="<?php echo htmlspecialchars($school_id); ?>">
+<input type="hidden" id="currentStudentId" value="<?php echo htmlspecialchars($studentId); ?>" />
+<input type="hidden" id="currentWeekStartDate" value="<?php echo htmlspecialchars($currentWeekStartDate); ?>" />
+
+<h1>Student Performance Data</h1>
+<button id="addDataRow">Add Data Row</button>
+
+<label for="startDateFilter">Filter by Start Date:</label>
+<input type="text" id="startDateFilter">
+
+<!-- Add the generated links here -->
+<?php foreach ($metadataEntries as $metadataEntry): ?>
+    <a href="?student_id=<?php echo $student_id; ?>&metadata_id=<?php echo $metadataEntry['metadata_id']; ?>">
+        <?php echo $metadataEntry['category_name']; ?>
+    </a><br>
+<?php endforeach; ?>
+
+<table border="1">
+<thead>
+    <tr>
+        <th>Date</th>
+        <?php 
+        // Iterate through all key-value pairs in $scoreNames.
+        foreach ($scoreNames as $category => $values) {
+            // Check if the current category's values are an array (assuming you only want arrays).
+            if (is_array($values)) {
+                // Iterate through each item in the current category's array.
+                foreach ($values as $score) {
+                    // Print the score as a table header. Apply any necessary formatting or escaping here.
+                    echo "<th>" . htmlspecialchars($score) . "</th>";
+                }
+            } else {
+                // If it's not an array, it might be a standalone category name. You can decide how to handle these cases.
+                // For example, you might want to print it as a header, too.
+                echo "<th>" . htmlspecialchars($values) . "</th>";
+            }
+        }
+        ?>
+        <th>Action</th>
+    </tr>
+</thead>
+
+    <?php if (empty($performanceData)): ?>
+        <tr>
+            <td colspan="11">No Data Found. Click "Add Data Row" to add new data.</td>
+        </tr>
+    <?php else: ?>
+        <?php foreach ($performanceData as $data): ?>
+            <tr data-performance-id="<?php echo $data['performance_id']; ?>">
+                <td class="editable" data-field-name="score_date">
+                    <?php
+                    if (isset($data['score_date'])) {
+                        echo date("m/d/Y", strtotime($data['score_date']));
+                    }
+                    ?>
+                </td>
+                <!-- Add scores using loop -->
+                <?php for ($i = 1; $i <= 10; $i++): ?>
+                    <td class="editable" data-field-name="score<?php echo $i; ?>">
+                        <?php
+                        if (isset($data['score'.$i])) {
+                            echo $data['score'.$i];
+                        }
+                        ?>
+                    </td>
+                <?php endfor; ?>
+                <td><button class="deleteRow" data-performance-id="<?php echo $data['performance_id']; ?>">Delete</button></td> <!-- New delete button for each row -->
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</table>
+<?php
+echo '<pre>';
+print_r($scoreNames);
+echo '</pre>';
+?>
+<label>Select Score to Display: </label>
+<select id="scoreSelector">
+    <?php 
+    // Assuming there might be more than one category like 'Writing Rubric' in the future,
+    // we're going through each. If there's only one, you can remove this outer foreach.
+    foreach ($scoreNames as $category => $scores) { 
+        // Check if the scores are indeed in an array, which they should be according to your structure.
+        if (is_array($scores)) { 
+            // Now, we can loop through the score names in the inner array.
+            foreach ($scores as $index => $scoreName) { ?>
+                <option value="<?php echo htmlspecialchars($category . '_' . $index); ?>"> <!-- Creating a composite value to maintain uniqueness -->
+                    <?php echo htmlspecialchars($scoreName); ?>
+                </option>
+            <?php }
+        } else {
+            // This part of the code handles the possibility that some entries in $scoreNames are not arrays.
+            // This is for future-proofing or in case your data structure has some variations.
+            ?>
+            <option value="<?php echo htmlspecialchars($category); ?>">
+                <?php echo htmlspecialchars($scores); ?> <!-- Assuming $scores is a string in this case -->
+            </option>
+            <?php
+        }
+    } ?>
+</select>
+
+
+
+
+<label>Enter Benchmark Value: </label>
+<input type="text" id="benchmarkValue">
+<button id="updateBenchmark">Update Benchmark</button>
+
+<div id="chart"></div>  <!-- Div to display the chart -->
+
+
+</body>
+</html>
---
+++ users/fetch_data.php
@@ -0,0 +1,199 @@
+<?php
+session_start();
+// Error Reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('db.php');
+
+function fetchPerformanceData($studentId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? ORDER BY score_date DESC LIMIT 41");
+    $stmt->execute([$studentId]);
+    return $stmt->fetchAll();
+}
+
+function fetchMetadataCategories($school_id) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT metadata_id, category_name FROM Metadata WHERE school_id = ?");
+    $stmt->execute([$school_id]);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function fetchSchoolIdForStudent($studentId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT school_id FROM Students WHERE student_id = ?");
+    $stmt->execute([$studentId]);
+    $result = $stmt->fetch();
+    return $result ? $result['school_id'] : null;
+}
+
+function fetchScoreNames($school_id) {
+    global $connection;
+    $scoreNames = [];
+    $stmt = $connection->prepare("SELECT ScoreColumn, CustomName FROM SchoolScoreNames WHERE school_id = ?");
+    $stmt->execute([$school_id]);
+    while ($row = $stmt->fetch()) {
+        $scoreNames[$row['ScoreColumn']] = $row['CustomName'];
+    }
+    return $scoreNames;
+}
+
+function fetchStudentsByTeacher($teacherId) {
+    global $connection;
+    $stmt = $connection->prepare("SELECT s.* FROM Students s INNER JOIN Teachers t ON s.school_id = t.school_id WHERE t.teacher_id = ?");
+    $stmt->execute([$teacherId]);
+    return $stmt->fetchAll();
+}
+
+function addNewStudent($studentName, $teacherId) {
+    global $connection;
+
+    // Fetch the school_id of the current teacher
+    $stmt = $connection->prepare("SELECT school_id FROM Teachers WHERE teacher_id = ?");
+    $stmt->execute([$teacherId]);
+    $teacherInfo = $stmt->fetch();
+    $teacherschool_id = $teacherInfo['school_id'];
+
+    // Check if the student with the same name and school_id already exists
+    $stmt = $connection->prepare("SELECT student_id FROM Students WHERE name = ? AND school_id = ?");
+    $stmt->execute([$studentName, $teacherschool_id]);
+    $duplicateStudent = $stmt->fetch();
+
+    if ($duplicateStudent) {
+        return "Student with the same name already exists.";
+    } 
+
+    // Insert the new student with the same school_id
+    $stmt = $connection->prepare("INSERT INTO Students (name, school_id) VALUES (?, ?)");
+    $stmt->execute([$studentName, $teacherschool_id]);
+    return "New student added successfully.";
+}
+
+function fetchGroupNames() {
+    global $connection;
+    $stmt = $connection->prepare("SELECT group_name FROM ScoreGroups");
+    $stmt->execute();
+    $stmt->bindColumn(1, $groupName);
+    
+    $groups = [];
+    while ($stmt->fetch(PDO::FETCH_BOUND)) {
+        $groups[] = $groupName;
+    }
+    
+    return $groups;
+}
+
+function getSmallestMetadataId($schoolId) {
+    global $connection;
+
+    // Prepare and execute a query to fetch the smallest metadata_id
+    $query = "SELECT MIN(metadata_id) AS smallest_metadata_id FROM Metadata WHERE school_id = :schoolId";
+    $stmt = $connection->prepare($query);
+    $stmt->bindParam(':schoolId', $schoolId, PDO::PARAM_INT);
+    $stmt->execute();
+
+    // Fetch the result
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Check if a result was found
+    if ($result && isset($result['smallest_metadata_id'])) {
+        return $result['smallest_metadata_id'];
+    } else {
+        return null; // No matching records found
+    }
+}
+// Initialize empty arrays and variables
+$performanceData = [];
+$scoreNames = [];
+$chartDates = [];
+$chartScores = [];
+
+// Check if the action is set to 'fetchGroups' and handle it
+if (isset($_GET['action']) && $_GET['action'] == 'fetchGroups') {
+    echo json_encode(fetchGroupNames());
+    exit;
+}
+
+// If student_id is not set, exit early
+if (!isset($_GET['student_id'])) {
+    return;
+}
+
+$studentId = $_GET['student_id'];
+$school_id = fetchSchoolIdForStudent($studentId);  // Fetch school_id
+
+if (!$school_id) {
+    return;  // If there's no school_id, exit early
+}
+
+
+if (!isset($_SESSION['teacher_id'])) {
+    die("Teacher ID not set in session");
+}
+
+$teacherId = $_SESSION['teacher_id'];
+$message = "";  // Initialize an empty message variable
+
+// Handle form submission for adding new student
+if (isset($_POST['add_new_student'])) {
+    $newStudentName = $_POST['new_student_name'];
+    if (!empty($newStudentName)) {
+        $message = addNewStudent($newStudentName, $teacherId);
+    }
+}
+
+$students = fetchStudentsByTeacher($teacherId);
+// Fetch performance data and score names
+$performanceData = fetchPerformanceData($studentId);
+$scoreNames = fetchScoreNames($school_id);
+
+// Preparing the data for the chart
+foreach ($performanceData as $record) {
+    $chartDates[] = $record['score_date'];
+    // You can add more logic here if needed
+}
+
+// Handling the data POST from the dropdown functionality
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ScoreGroup'])) {
+    $school_idIndex = $_POST['school_idIndex'];
+    $originalName = $_POST['ScoreColumn'];
+    $customName = $_POST['CustomName'];
+    $scoreGroup = $_POST['ScoreGroup'];
+
+    // Inserting into the SchoolScoreNames table
+    $stmt = $connection->prepare("INSERT INTO SchoolScoreNames (school_idIndex, ScoreColumn, CustomName, group_name) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$school_idIndex, $originalName, $customName, $scoreGroup]);
+    
+    // Respond with the ID of the inserted row
+    echo json_encode(['id' => $connection->lastInsertId()]);
+    exit;
+}
+
+// Fetch metadata entries from the Metadata table for the specified school_id
+$stmt = $connection->prepare("SELECT metadata_id, category_name FROM Metadata WHERE school_id = ?");
+$stmt->execute([$school_id]);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $metadataEntries[] = $row;
+}
+
+// Checking and setting the $student_id
+if (isset($_GET['student_id'])) {
+    $student_id = $_GET['student_id'];
+} else {
+    $student_id = null; // or set a default value appropriate for your context
+}
+
+// Output the links to tables for each metadata entry
+foreach ($metadataEntries as $metadataEntry) {
+    $metadata_id = $metadataEntry['metadata_id'];
+    $categoryName = $metadataEntry['category_name'];
+    // Generate a link to the table for this metadata entry
+}
+
+$stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? AND metadata_id = ? ORDER BY score_date DESC LIMIT 41");
+$stmt->execute([$studentId, $metadata_id]);
+
+?>
+
