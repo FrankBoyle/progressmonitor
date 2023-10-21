6e58ab888ed7dcae081f2af39162b8b130fe1d40
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-20 22:51:00 -0400

    Update fetch_data.php

--- testing/users/fetch_data.php
+++ testing/users/fetch_data.php
@@ -7,6 +7,14 @@ error_reporting(E_ALL);
 
 include('db.php');
 
+// Check if the teacher is logged in
+if (!isset($_SESSION['teacher_id'])) {
+    die("Teacher ID not set in session");
+}
+
+$teacherId = $_SESSION['teacher_id'];
+$message = "";  // Initialize an empty message variable
+
 function fetchPerformanceData($studentId, $metadata_id) {
     global $connection;
     $stmt = $connection->prepare("SELECT * FROM Performance WHERE student_id = ? AND metadata_id = ? ORDER BY score_date DESC LIMIT 41");
@@ -137,48 +145,38 @@ function getSmallestMetadataId($schoolId) {
         return null; // No matching records found
     }
 }
+
+// Ensure studentId is provided, else we can't proceed
+if (!isset($_GET['student_id'])) {
+    die("Student ID not provided");
+}
+
 // Initialize empty arrays and variables
 $performanceData = [];
 $scoreNames = [];
 $chartDates = [];
 $chartScores = [];
-$studentId = $_POST['student_id'];
+$metadata_id = $_GET['metadata_id'];
+$studentId = $_GET['student_id'];
+$school_id = fetchSchoolIdForStudent($studentId);  // Fetch school_id
+if (!$school_id) {
+    die("No school found for this student");  // If there's no school_id, exit early
+}
 //$metadata_id = $_POST['metadata_id']; // Get metadata_id from POST
 //$schoolId = $_POST['school_id']; // Get school_id from POST
 //$scores = $_POST['scores'];
-$metadata_id = $_GET['metadata_id'];
+
 // Check if the action is set to 'fetchGroups' and handle it
 if (isset($_GET['action']) && $_GET['action'] == 'fetchGroups') {
     echo json_encode(fetchGroupNames());
     exit;
 }
 
-// If student_id is not set, exit early
-if (!isset($_GET['student_id'])) {
-    return;
-}
-
-$studentId = $_GET['student_id'];
-$school_id = fetchSchoolIdForStudent($studentId);  // Fetch school_id
-
-if (!$school_id) {
-    return;  // If there's no school_id, exit early
-}
-
-
-if (!isset($_SESSION['teacher_id'])) {
-    die("Teacher ID not set in session");
-}
-
-$teacherId = $_SESSION['teacher_id'];
-$message = "";  // Initialize an empty message variable
-
-// Handle form submission for adding new student
-if (isset($_POST['add_new_student'])) {
+// Handle the form submission for adding a new student, if applicable
+if (isset($_POST['add_new_student']) && !empty($_POST['new_student_name'])) {
     $newStudentName = $_POST['new_student_name'];
-    if (!empty($newStudentName)) {
-        $message = addNewStudent($newStudentName, $teacherId);
-    }
+    $message = addNewStudent($newStudentName, $teacherId);
+    // Consider redirecting or providing output after this action.
 }
 
 $students = fetchStudentsByTeacher($teacherId);
@@ -213,13 +211,6 @@ $stmt = $connection->prepare("SELECT metadata_id, category_name FROM Metadata WH
 $stmt->execute([$school_id]);
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $metadataEntries[] = $row;
-}
-
-// Checking and setting the $student_id
-if (isset($_GET['student_id'])) {
-    $student_id = $_GET['student_id'];
-} else {
-    $student_id = null; // or set a default value appropriate for your context
 }
 
 // Output the links to tables for each metadata entry
