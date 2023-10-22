afbdeb370f6215cc88e8187e4667de20ee3436f2
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-22 19:08:26 -0400

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -39,7 +39,7 @@ $("input[name='selectedColumns[]']").click(function() {
         selectedColumns.push($(this).val()); // Add the selected columns to the array
     });
     var selectedScore = $("#scoreSelector").val();
-    updateChart(selectedScore, selectedColumns); // Pass the array of selected columns
+    updateChart(selectedScore); // Pass the array of selected columns
 });
 
 
