92c8ff278f349c948b55fc413a33507e0da2e493
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-22 09:43:39 -0400

    a

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -41,7 +41,7 @@ error_reporting(E_ALL);
 </head>
 <body>
 <h1>Student Overview</h1>
-<p>Name: <?php echo htmlspecialchars($student_name); ?></p>
+<p>Name: <?php echo $studentName; ?></p>
 <p>Category: <?php echo $metadataEntry['category_name']; ?></p>
 
 <a href="test.php" class="btn btn-primary">Student List</a>
