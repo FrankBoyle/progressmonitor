6a7b6e6a208b0f56a8770bcb19a5d4bbc7a2bd35
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-10-27 08:27:54 -0400

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -598,6 +598,31 @@ $(document).ready(function() {
         }, 'json');
     });
 
+    function updateScoreInDatabase(row, metadataFieldName, newValue) {
+        const performanceId = row.data('performance-id');
+        const studentId = CURRENT_STUDENT_ID;
+        const score_date = row.find('td[data-field-name="score_date"]').text();
+        
+        // Assuming that metadataFieldName would be something like "score1_name" and we'd need to update "score1"
+        const fieldNameToUpdate = metadataFieldName.replace('_name', '');
+    
+        const postData = {
+            performance_id: performanceId,
+            field_name: fieldNameToUpdate, // Use the extracted field name to update the appropriate score column
+            new_value: newValue,
+            student_id: studentId,
+            score_date: score_date
+            
+        };
+    
+        ajaxCall('POST', 'update_performance.php', postData).then(response => {
+            if (response && !response.success) {
+                console.error('Error updating the score in the database.');
+            }
+        });
+    }
+    
+
     function isDateDuplicate(dateString, currentPerformanceId = null, currentStudentId = null, currentMetadataId = null) {
         //console.log("Checking for duplicate of:", dateString);
         let isDuplicate = false;
@@ -739,9 +764,8 @@ $('#addDataRow').off('click').click(function() {
     }
     
     const currentDate = getCurrentDate();
-
 if (isDateDuplicate(currentDate)) {
-    alert("An entry for this date already exists. Please choose a different date.");
+    //alert("An entry for this date already exists. Please choose a different date.");
     return;
 }
         const newRow = $("<tr data-performance-id='new'>");
@@ -799,7 +823,7 @@ if (isDateDuplicate(currentDate)) {
         };
     
         if (isDateDuplicate(postData.score_date)) {
-            alert("An entry for this date already exists. Please choose a different date.");
+           // alert("An entry for this date already exists. Please choose a different date.");
             return;
         }
     
