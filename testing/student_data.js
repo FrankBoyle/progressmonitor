c26286008b370693eb7460f1e00c4501906ccd68
FrankBoyle <francisjboyleiii@gmail.com>
2023-10-31 20:01:11 -0400

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -76,12 +76,8 @@ document.addEventListener("DOMContentLoaded", function() {
             chart = new ApexCharts(document.querySelector("#chart"), options);
             chart.render();
         } else {
-            if (chart) {
-                chart.destroy();
-            }
-            options.series = newSeriesData;
-            chart = new ApexCharts(document.querySelector("#chart"), options);
-            chart.render();
+            document.querySelector("#chart svg").innerHTML = '';
+            chart.updateSeries(newSeriesData);
                     }
     }, 250));  // Here's where the 250 millisecond delay is applied   
 });
