86c0980fafe9bab1633ff56e9e0a635218cea087
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-05 10:00:50 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -275,10 +275,10 @@ function getChartOptions(dates, trendlineSeriesData) {
             dropShadow: {
                 enabled: true,
                 color: '#000',
-                top: 15,  // Change to 0 to see if positioning is the issue
-                left: 15,  // Change to 0 for same reason
-                blur: 7,  // Increase blur for visibility
-                opacity: 0.5  // Increase opacity for visibility
+                top: 10,  // Change to 0 to see if positioning is the issue
+                left: 10,  // Change to 0 for same reason
+                blur: 10,  // Increase blur for visibility
+                opacity: 0.4  // Increase opacity for visibility
             },
         },
         colors: seriesColors,
