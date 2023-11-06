8aee6a462e26b99189fae44334c645120ce7b7ef
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-11-06 14:35:26 -0500

    Update starter.php

--- testing/starter.php
+++ testing/starter.php
@@ -368,8 +368,8 @@ $students = fetchStudentsByTeacher($teacherId);
             <!-- /.card -->
 
 
-                <!--<a href="#" class="card-link">Card link</a>-->
-                <!--<a href="#" class="card-link">Another link</a>-->
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
               </div>
             </div><!-- /.card -->
           </div>
