0e5c87dcf84c6075674ceba6e767586d7900d7d3
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-11-06 14:29:03 -0500

    Update starter.php

--- testing/starter.php
+++ testing/starter.php
@@ -300,26 +300,31 @@ $students = fetchStudentsByTeacher($teacherId);
     <!-- /.content-header -->
 
     <!-- Main content -->
-    <?php
-include './users/fetch_students.php';
+  
+    <!-- Add New Student Form -->
+    <form method="post" action="">
+        <label for="new_student_name">New Student Name:</label>
+        <input type="text" id="new_student_name" name="new_student_name">
+        <input type="submit" name="add_new_student" value="Add New Student">
+    </form>
 
-if (!isset($_SESSION['teacher_id'])) {
-    die("Teacher ID not set in session");
-}
+    <?php if ($message): ?>
+        <p><?= $message ?></p>
+    <?php endif; ?>
 
-$teacherId = $_SESSION['teacher_id'];
-$message = "";  // Initialize an empty message variable
+    <?php if (!empty($students)): ?>
+        <h2>Students:</h2>
+        <?php foreach ($students as $student): ?>
+            <?php
+                // Fetch the smallest metadata_id for the student's school_id
+                $metadataId = getSmallestMetadataId($student['school_id']);
+            ?>
+            <a href='view_student_data.php?student_id=<?= $student['student_id'] ?>&metadata_id=<?= $metadataId ?>'><?= $student['name'] ?></a><br>
+        <?php endforeach; ?>
+    <?php else: ?>
+        No students found for this teacher.
+    <?php endif; ?>
 
-// Handle form submission for adding new student
-if (isset($_POST['add_new_student'])) {
-    $newStudentName = $_POST['new_student_name'];
-    if (!empty($newStudentName)) {
-        $message = addNewStudent($newStudentName, $teacherId);
-    }
-}
-
-$students = fetchStudentsByTeacher($teacherId);
-?>
     <div class="content">
       <div class="container-fluid">
               <div class="card-body">
