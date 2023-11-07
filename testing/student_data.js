12a773628b5720ece6da99bbaaa8ce9ef8d125ee
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-07 18:05:31 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -854,6 +854,17 @@ $(document).ready(function() {
         }
     }
     
+    
+    function toggleEditMode(cell, input) {
+        if (cell.hasClass('editing')) {
+            cell.removeClass('editing');
+            input.hide();
+        } else {
+            cell.addClass('editing');
+            input.show();
+        }
+    }
+    
 
     $('#addDataRow').off('click').click(function() {
         const currentDate = getCurrentDate();
