d98fd25e255a1c769021defb9e1aecce8a5a56cf
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-06 19:55:12 -0500

    Update starter.php

--- testing/starter.php
+++ testing/starter.php
@@ -435,6 +435,7 @@ $students = fetchStudentsByTeacher($teacherId);
 
 <!-- jQuery -->
 
+<script src="./plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
