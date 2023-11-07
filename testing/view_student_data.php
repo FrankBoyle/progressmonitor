1492c6755c3354e41b06806f8004afb968a33332
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-06 22:08:25 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -109,7 +109,7 @@ if (isset($_GET['metadata_id'])) {
 </div>
 
 <!-- Snippet of the modified code -->
-<table border="1" id="dataTable" class="table">
+<table border="1" id="dataTable">
     <thead>
         <tr>
             <th>Date</th>
