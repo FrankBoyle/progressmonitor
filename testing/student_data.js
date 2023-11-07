55d48dc7500d35935fb8ef89da4b168eab6bc3f1
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-06 21:25:31 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -986,4 +986,16 @@ $.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
             var date = data.split('/');
             return (date[2] + date[0] + date[1]) * 1;
         };
+
+        let table = $('table').DataTable({
+            "order": [[0, "asc"]],
+            "lengthChange": false,
+            //"searching": false,
+            "paging": false,
+            "info": false,
+            "columns": [
+                { "type": "date-us" },
+                null, null, null, null, null, null, null, null, null, null, null
+            ]
+        });
 });
