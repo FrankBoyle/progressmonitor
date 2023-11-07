6faa123bebaad3790cb2d3428bead34d94761053
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-06 19:25:42 -0500

    Update starter.php

--- testing/starter.php
+++ testing/starter.php
@@ -300,7 +300,27 @@ $students = fetchStudentsByTeacher($teacherId);
     <!-- /.content-header -->
 
     <!-- Main content -->
-  
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-outline card-info">
+            <div class="card-header">
+              <h3 class="card-title">
+                Summernote
+              </h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <textarea id="summernote">
+                Place <em>some</em> <u>text</u> <strong>here</strong>
+              </textarea>
+            </div>
+            <div class="card-footer">
+              Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+            </div>
+          </div>
+        </div>
     <!-- Add New Student Form -->
     <form method="post" action="">
         <label for="new_student_name">New Student Name:</label>
