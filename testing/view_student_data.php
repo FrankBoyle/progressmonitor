f282f02b9ec610080ff48e37f75f4764862a8cec
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-06 21:11:54 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -180,10 +180,10 @@ if (isset($_GET['metadata_id'])) {
 <script>
   $(function () {
     // Initialize DataTable for the table with ID 'dataTable'
-    $("#example1").DataTable({
+    $("#dataTable").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    }).buttons().container().appendTo('#dataTable_wrapper .col-md-6:eq(0)');
   });
 </script>
 </body>
