daab16746524bf87e130cae7e4b66b35a9d029bf
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-16 17:56:15 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -108,14 +108,239 @@ if (isset($_GET['metadata_id'])) {
 </style>
 
 </head>
-<body>
-<h1>Student Performance Data</h1>
 
-<h2>Student Overview</h2>
-<p>Name: <?php echo $studentName; ?></p>
-<p>Category: <?php echo $selectedCategoryName; ?></p>
+
+<body>
+      <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="./plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- jsGrid -->
+<link rel="stylesheet" href="./plugins/jsgrid/jsgrid.min.css"> 
+<link rel="stylesheet" href="./plugins/jsgrid/jsgrid-theme.min.css"> 
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="./plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="./plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="./plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="./plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css">
+
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item dropdown d-none d-sm-inline-block">
+      <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-envelope"></i></a>
+            <div class="dropdown-menu">
+              <a href="mailto: sales@bfactor.org" class="dropdown-item">
+                <span href="#" class="dropdown-item">Sales</button>
+                </a>
+            <div class="dropdown-divider"></div>
+              <a href="mailto: support@bfactor.org" class="dropdown-item">
+                <span href="#" class="dropdown-item">Support</span>
+              </a>
+            </div>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Bfactor</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block"><?php echo($_SESSION['user']);?></a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-closed">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Starter Pages
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./home.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Students</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Inactive Page</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Simple Link
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1><?php echo $studentName; ?> <?php echo $selectedCategoryName; ?> Performance Data</h1>
+            <a href="test.php" class="btn btn-primary">Home</a>  
+        </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="./test.php">Home</a></li>
+              <li class="breadcrumb-item active">Text Editors</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+  <div class="container-fluid">
+    <h5 class="mb-2">GOALS</h5>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 1</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 2</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>   
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 3</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </div>
+</section>
+
 <div>
-<a href="test.php" class="btn btn-primary">Student List</a>
 <input type="hidden" id="schoolIdInput" name="school_id" value="<?php echo htmlspecialchars($school_id); ?>">
 <input type="hidden" id="currentStudentId" value="<?php echo htmlspecialchars($student_id); ?>" />
 <input type="hidden" id="currentWeekStartDate" value="<?php echo htmlspecialchars($currentWeekStartDate); ?>" />
