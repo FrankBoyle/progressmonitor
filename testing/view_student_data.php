da95385097afced24782cfc06431f8b2e0bc589f
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-16 18:00:09 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -268,11 +268,11 @@ if (isset($_GET['metadata_id'])) {
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1><?php echo $studentName; ?> <?php echo $selectedCategoryName; ?> Performance Data</h1>
-            <a href="test.php" class="btn btn-primary">Home</a>  
+            <a href="home.php" class="btn btn-primary">Home</a>  
         </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="./test.php">Home</a></li>
+              <li class="breadcrumb-item"><a href="./home.php">Home</a></li>
               <li class="breadcrumb-item active">Text Editors</li>
             </ol>
           </div>
