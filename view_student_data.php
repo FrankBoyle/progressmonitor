d634cc59bb763a82edcb9254c28cab348be279cd
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-18 15:35:03 -0500

    Update view_student_data.php

--- view_student_data.php
+++ view_student_data.php
@@ -38,31 +38,42 @@ if (isset($_GET['metadata_id'])) {
 <head>
     <meta charset="UTF-8">
     <title><?php echo $studentName; ?></title>
-  <!-- DataTables -->
-
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    <!-- DataTables -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-            <!-- Bootstrap 4 -->
-<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css">
+    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
 
-<!-- DataTables  & Plugins -->
-<script src="./plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="./plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="./plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="./plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="./plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="./plugins/jszip/jszip.min.js"></script>
-<script src="./plugins/pdfmake/pdfmake.min.js"></script>
-<script src="./plugins/pdfmake/vfs_fonts.js"></script>
-<script src="./plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="./plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="./plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+   
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
     <script src="student_data.js"  defer></script>
     
     <script>
