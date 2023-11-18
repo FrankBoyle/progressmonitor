0d7857b9f7ff546bd78e142d455ab689889f95ba
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-18 15:38:59 -0500

    Update view_student_data.php

--- view_student_data.php
+++ view_student_data.php
@@ -38,20 +38,22 @@ if (isset($_GET['metadata_id'])) {
 <head>
     <meta charset="UTF-8">
     <title><?php echo $studentName; ?></title>
-    <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
     <!-- DataTables -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.41.1/dist/apexcharts.min.css">
     <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
 
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+   
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
@@ -83,10 +85,10 @@ if (isset($_GET['metadata_id'])) {
     </script>
 
 <style>
-#dataTable_wrapper .col-md-6:eq(0) {
-    position: relative;
-    z-index: 1000;
-}
+    #dataTable_wrapper .col-md-6:eq(0) {
+        position: relative;
+        z-index: 1000;
+    }
 
     .editable {
         cursor: pointer;
@@ -104,24 +106,224 @@ if (isset($_GET['metadata_id'])) {
         background-color: transparent;
         outline: none;
     }
+
+    .fixed-width {
+        width: 100px; /* Set your desired width here */
+    }
+
+    .fixed-width-cell {
+        width: 100px; /* Set your desired width here */
+        white-space: nowrap; /* Prevent text from wrapping */
+        padding: 0;
+        margin: 0;
+    }
 </style>
 
 </head>
-<body>
-<h1>Student Performance Data</h1>
+<body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
 
-<h2>Student Overview</h2>
-<p>Name: <?php echo $studentName; ?></p>
-<p>Category: <?php echo $selectedCategoryName; ?></p>
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item dropdown d-none d-sm-inline-block">
+      <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-envelope"></i></a>
+            <div class="dropdown-menu">
+              <a href="mailto: sales@bfactor.org" class="dropdown-item">
+                <span href="#" class="dropdown-item">Sales</button>
+                </a>
+            <div class="dropdown-divider"></div>
+              <a href="mailto: support@bfactor.org" class="dropdown-item">
+                <span href="#" class="dropdown-item">Support</span>
+              </a>
+            </div>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Bfactor</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block"><?php echo($_SESSION['user']);?></a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-closed">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fabtn-block-tachometer-alt"></i>
+              <p>
+                Starter Pages
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./home.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Students</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Inactive Page</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Simple Link
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1><?php echo $studentName; ?> Performance Data - <?php echo $selectedCategoryName; ?></h1>
+            <a href="home.php" class="btn btn-primary">Home</a>  
+        </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="./home.php">Home</a></li>
+              <li class="breadcrumb-item active">Text Editors</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+  <div class="container-fluid">
+    <h5 class="mb-2">GOALS</h5>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 1</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote1" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 2</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote2" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>   
+      <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+          <!-- If you want to add an icon to the box, uncomment this
+          <span class="info-box-icon bg-info"><i class="far fa-star"></i></span>
+          -->
+          <div class="info-box-content">
+            <span class="info-box-text">Goal 3</span>
+            <!-- Summernote editor -->
+            <textarea id="summernote3" class = "goaltext">
+              Place <em>some</em> <u>text</u> <strong>here</strong>
+            </textarea>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </div>
+</section>
+
 <div>
-<a href="test.php" class="btn btn-primary">Student List</a>
 <input type="hidden" id="schoolIdInput" name="school_id" value="<?php echo htmlspecialchars($school_id); ?>">
 <input type="hidden" id="currentStudentId" value="<?php echo htmlspecialchars($student_id); ?>" />
-</div>
-
-<div>
-<label for="startDateFilter">Filter by Start Date:</label>
-<input type="text" id="startDateFilter">
+<input type="hidden" id="currentWeekStartDate" value="<?php echo htmlspecialchars($currentWeekStartDate); ?>" />
 </div>
 
 <div>
@@ -168,14 +370,14 @@ if (isset($_GET['metadata_id'])) {
                             <?php echo isset($data['score'.$i]) ? $data['score'.$i] : ""; ?>
                         </td>
                     <?php endfor; ?>
-                    <td><button class="deleteRow" data-performance-id="<?php echo $data['performance_id']; ?>">Delete</button></td>
+                    <td><button class="deleteRow btn btn-block btn-primary" data-performance-id="<?php echo $data['performance_id']; ?>">Delete</button></td>
                 </tr>
             <?php endforeach; ?>
         <?php endif; ?>
     </tbody>
 </table>
 
-<button id="addDataRow">Add Data Row</button>
+<button id="addDataRow" class="btn btn-primary">Add Data Row</button>
 <input type="text" id="newRowDate" style="display: none;">
 
 <!-- Existing checkboxes for column selection -->
@@ -199,7 +401,7 @@ if (isset($_GET['metadata_id'])) {
 
 <label>Enter Benchmark Value:</label>
 <input type="text" id="benchmarkValue">
-<button type ="button" id="updateBenchmark">Update Benchmark</button>
+<button type ="button" id="updateBenchmark" class="btn btn-primary">Update Benchmark</button>
 
 <div id="accordion">
     <h3>Line Graph</h3>
@@ -212,8 +414,18 @@ if (isset($_GET['metadata_id'])) {
     </div>
 </div>
 
-<script>
+  <script>
+    $(document).ready(function() {
+      $('.goaltext').summernote({
+        toolbar: [
+          // Only include buttons for font type and basic styling
+          ['font', ['fontname']], // Font type
+          ['style', ['bold', 'italic', 'underline']] // Bold, italic, underline
+        ],
+        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'] // Add custom font types if needed
+      });
+    });
+  </script>
 
-</script>
 </body>
 </html>
