f1bf72acaa2ef7f487c73815894e17eeee897816
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-18 19:16:53 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -523,10 +523,6 @@ if (isset($_GET['metadata_id'])) {
 <!-- ./wrapper -->
 
 <!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
 
