e4baed11ef081904db235b0e626fea4cdb414829
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-20 17:26:56 -0500

    Update add_new_goal.php

--- testing/add_new_goal.php
+++ testing/add_new_goal.php
@@ -1,6 +1,8 @@
 <?php
 // Include the database connection script
 include('./users/db.php');
+
+// Set the Content-Type to application/json
 header('Content-Type: application/json');
 
 // Turn on error reporting for debugging. Remember to turn this off in production
@@ -15,29 +17,42 @@ function handleError($errorMessage) {
 }
 
 // Check if the required POST data is present
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['goal_description']) && isset($_POST['student_id']) && isset($_POST['metadata_id']) && isset($_POST['school_id'])) {
-    $goalDescription = $_POST['goal_description'];
-    $studentId = $_POST['student_id'];
-    $metadataId = $_POST['metadata_id'];
-    $schoolId = $_POST['school_id'];
-    $goalDate = isset($_POST['goal_date']) && !empty($_POST['goal_date']) ? $_POST['goal_date'] : null;
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['goal_description'])) {
+    // Extract and sanitize input
+    $goalDescription = filter_var($_POST['goal_description'], FILTER_SANITIZE_STRING);
+    $studentId = filter_var($_POST['student_id'], FILTER_SANITIZE_NUMBER_INT);
+    $metadataId = filter_var($_POST['metadata_id'], FILTER_SANITIZE_NUMBER_INT);
+    $schoolId = filter_var($_POST['school_id'], FILTER_SANITIZE_NUMBER_INT);
+    $goalDate = !empty($_POST['goal_date']) ? $_POST['goal_date'] : null;
 
-    // Prepare and bind
+    // Prepare the SQL statement
     $stmt = $connection->prepare("INSERT INTO Goals (student_id, goal_description, school_id, metadata_id, goal_date) VALUES (?, ?, ?, ?, ?)");
+
+    // Bind the parameters
     $stmt->bindParam(1, $studentId, PDO::PARAM_INT);
     $stmt->bindParam(2, $goalDescription, PDO::PARAM_STR);
     $stmt->bindParam(3, $schoolId, PDO::PARAM_INT);
     $stmt->bindParam(4, $metadataId, PDO::PARAM_INT);
-    $stmt->bindParam(5, $goalDate, PDO::PARAM_STR);
+    $stmt->bindParam(5, $goalDate); // Assuming goal_date can be NULL or a string
 
+    // Execute the statement and check for success
     if ($stmt->execute()) {
-        echo json_encode(['success' => true, 'goal_id' => $connection->lastInsertId()]);
+        $newGoalId = $connection->lastInsertId();
+        echo json_encode([
+            'success' => true, 
+            'goal_id' => $newGoalId,
+            'goal_description' => $goalDescription
+        ]);
     } else {
-        handleError('Database insertion failed: ' . implode(" | ", $stmt->errorInfo()));
+        handleError('Database insertion failed: ' . $stmt->errorInfo()[2]); // [2] is the actual error message
     }
+
+    // Close the statement
+    $stmt = null;
 } else {
     handleError('Invalid request. Missing required fields.');
 }
 ?>
+
 
 
