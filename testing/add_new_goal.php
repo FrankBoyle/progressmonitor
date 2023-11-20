bc71f5a846b70cb344418a3aa950da7c03a303be
FrankBoyle <francisjboyleiii@gmail.com>
2023-11-20 16:50:41 -0500

    Update add_new_goal.php

--- testing/add_new_goal.php
+++ testing/add_new_goal.php
@@ -3,26 +3,32 @@
 include('./users/db.php');
 header('Content-Type: application/json');
 
+// Disable error display, log errors instead
 ini_set('display_errors', 0);
-ini_set('display_startup_errors', 1);
 ini_set('log_errors', 1);
 error_reporting(E_ALL);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['goal_description'])) {
+// Check if the required POST data is present
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['goal_description']) && isset($_POST['student_id']) && isset($_POST['metadata_id']) && isset($_POST['school_id'])) {
     $goalDescription = $_POST['goal_description'];
+    $studentId = $_POST['student_id'];
+    $metadataId = $_POST['metadata_id'];
+    $schoolId = $_POST['school_id'];
+    $goalDate = isset($_POST['goal_date']) ? $_POST['goal_date'] : date('Y-m-d'); // Use current date if not provided
 
     // Prepare and bind
-    $stmt = $connection->prepare("INSERT INTO Goals (goal_description) VALUES (?)");
-    $stmt->bind_param("s", $goalDescription);
+    $stmt = $connection->prepare("INSERT INTO Goals (student_id, goal_description, goal_date, school_id, metadata_id) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("issii", $studentId, $goalDescription, $goalDate, $schoolId, $metadataId);
 
     if ($stmt->execute()) {
-        echo json_encode(['success' => true]);
+        echo json_encode(['success' => true, 'goal_id' => $connection->insert_id]); // Return the new goal ID
     } else {
-        echo json_encode(['success' => false, 'message' => 'Database insertion failed.']);
+        echo json_encode(['success' => false, 'message' => 'Database insertion failed: ' . $stmt->error]);
     }
 
     $stmt->close();
 } else {
-    echo json_encode(['success' => false, 'message' => 'Invalid request.']);
+    echo json_encode(['success' => false, 'message' => 'Invalid request. Missing required fields.']);
 }
 ?>
+
