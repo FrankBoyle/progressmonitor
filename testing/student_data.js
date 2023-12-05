9f8dd33c9a4d2a415c22ae699b787aa163076f58
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-12-05 09:03:29 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -310,9 +310,9 @@ function getChartOptions(dates, trendlineSeriesData) {
             dropShadow: {
                 enabled: true,
                 color: seriesColors,
-                top: 0,  // Change to 0 to see if positioning is the issue
-                left: 0,  // Change to 0 for same reason
-                blur: 3,  // Increase blur for visibility
+                top: 7,  // Change to 0 to see if positioning is the issue
+                left: 7,  // Change to 0 for same reason
+                blur: 4,  // Increase blur for visibility
                 opacity: 0.2  // Increase opacity for visibility
             },
         },
