1c1413e782d97f4cdaec574c46a2d1499cb636ea
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-12-05 09:04:13 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -311,9 +311,9 @@ function getChartOptions(dates, trendlineSeriesData) {
                 enabled: true,
                 color: seriesColors,
                 top: 7,  // Change to 0 to see if positioning is the issue
-                left: 7,  // Change to 0 for same reason
+                left: 4,  // Change to 0 for same reason
                 blur: 4,  // Increase blur for visibility
-                opacity: 0.2  // Increase opacity for visibility
+                opacity: 0.1  // Increase opacity for visibility
             },
         },
         colors: seriesColors,
