ca6510c4c6fb371849a7f5e37dad18668632c496
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-12-06 11:35:46 -0500

    Update view_student_data.php

--- testing/view_student_data.php
+++ testing/view_student_data.php
@@ -601,27 +601,55 @@ $('.goal-checkbox').change(function() {
     }
 });
 
-document.getElementById('printButton').addEventListener('click', function() {
-    var selectedGoal = getSelectedGoalContent();
-    var graphContent = document.getElementById('chart').outerHTML; // Get the graph content
-    var notesContent = document.getElementById('graphNotes').value; // Get the notes content
+    });
 
-    var printWindow = window.open('', '_blank');
-    printWindow.document.write('<html><head><title>Print</title><link rel="stylesheet" href="path/to/your/print-stylesheet.css"></head><body>');
-    printWindow.document.write(selectedGoal + graphContent + notesContent);
-    printWindow.document.write('</body></html>');
-    printWindow.document.close();
-    printWindow.focus();
-    setTimeout(() => printWindow.print(), 1000); // Delay to ensure content loads
+    $('#printButton').click(function() {
+    var selectedGoalContent = getSelectedGoalContent();
+    
+    getGraphContentAsImage('#chart', function(graphImage) {
+        var notesContent = $('#graphNotes').summernote('code');
+        var contentToPrint = '<div>' + selectedGoalContent + '</div>' +
+                             '<img src="' + graphImage + '">' +
+                             '<div>' + notesContent + '</div>';
+
+        printContent(contentToPrint);
+    });
 });
 
+
 function getSelectedGoalContent() {
-    // Assuming selected goal has a specific class like 'selected'
-    var selectedGoal = document.querySelector('.goal-container.selected');
-    return selectedGoal ? selectedGoal.outerHTML : 'No goal selected';
+    // Assuming selected goal has a specific class like 'selected-goal'
+    var selectedGoal = $('.selected-goal');
+    return selectedGoal.length > 0 ? selectedGoal.html() : 'No goal selected';
 }
 
-    });
+function getGraphContentAsImage(chartSelector, callback) {
+    var chart = $(chartSelector).data('apexcharts');
+    if (chart) {
+        chart.dataURI().then(({ imgURI }) => {
+            callback(imgURI);
+        });
+    } else {
+        callback(''); // No chart found
+    }
+}
+
+// Usage example
+getGraphContentAsImage('#chart', function(imageData) {
+    // Use imageData as the src for an img tag or however you need it
+    console.log(imageData); // This is your base64 image
+});
+
+
+function printContent(content) {
+    var printWindow = window.open('', '_blank');
+printWindow.document.write('<html><head><title>Print</title></head><body>');
+printWindow.document.write(content);
+printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+  
   </script>
 
 </body>
