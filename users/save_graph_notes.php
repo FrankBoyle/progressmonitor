a99c0119662597fa04b934bc0a552b5a4ca75a72
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2023-12-13 13:31:11 -0500

    Update save_graph_notes.php

--- users/save_graph_notes.php
+++ users/save_graph_notes.php
@@ -8,10 +8,20 @@ function addNotes($goalId, $studentId, $schoolId, $metadataId, $notes) {
     global $connection; // Assuming you have a database connection
 
     try {
-        // Prepare the SQL statement to insert notes
-        $stmt = $connection->prepare("INSERT INTO Goal_notes (goal_id, student_id, school_id, metadata_id, notes) VALUES (?, ?, ?, ?, ?)
-                                      ON DUPLICATE KEY UPDATE notes = ?, student_id = ?, school_id = ?, metadata_id = ?");
-        $stmt->execute([$goalId, $studentId, $schoolId, $metadataId, $notes, $notes, $studentId, $schoolId, $metadataId]);
+        // Check if the notes already exist for the given goal
+        $stmt = $connection->prepare("SELECT COUNT(*) FROM Goal_notes WHERE goal_id = ?");
+        $stmt->execute([$goalId]);
+        $count = $stmt->fetchColumn();
+
+        if ($count > 0) {
+            // Update the existing notes
+            $stmt = $connection->prepare("UPDATE Goal_notes SET notes = ? WHERE goal_id = ?");
+            $stmt->execute([$notes, $goalId]);
+        } else {
+            // Insert new notes
+            $stmt = $connection->prepare("INSERT INTO Goal_notes (goal_id, student_id, school_id, metadata_id, notes) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute([$goalId, $studentId, $schoolId, $metadataId, $notes]);
+        }
 
         echo json_encode(['status' => 'success', 'message' => 'Notes added successfully.']);
     } catch (PDOException $e) {
