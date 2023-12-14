09921489c79443886e5c76990663a6cdffa76aa9
FrankBoyle <francisjboyleiii@gmail.com>
2023-12-13 21:21:30 -0500

    Update home.php

--- testing/home.php
+++ testing/home.php
@@ -173,13 +173,9 @@ function extractLastName($fullName) {
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
+              <a href="./users/logout.php" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>Sign Out<span class="right badge badge-danger"></span></p>
           </li>
         </ul>
       </nav>
