9bf3ab17cd13647930cef21141a87afd273f6994
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-01-23 12:41:33 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -763,7 +763,7 @@ $(document).ready(function() {
                                 input.val(originalValue);
                                 console.log(originalValue);
                             } else {
-                                saveEditedDate(cell, selectedDate);
+                                saveEditedDate(cell, convertedValue);
                             }
                         }
                         toggleEditMode(cell, input);
@@ -780,7 +780,7 @@ $(document).ready(function() {
             input.on('keydown', function(e) {
                 if (e.keyCode === 13) { // Enter key pressed
                     e.preventDefault();
-                    saveEditedDate(cell, selectedDate);
+                    saveEditedDate(cell, convertedValue);
                 }
             });
     
