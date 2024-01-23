90b4d0ebdc7d29eebd63623614fc9a06a5256ab3
FrankBoyle <francisjboyleiii@gmail.com>
2024-01-22 19:20:57 -0500

    Update student_data.js

--- testing/student_data.js
+++ testing/student_data.js
@@ -814,6 +814,7 @@ $(document).ready(function() {
         }
     
         if (cell.data('field-name') === 'score_date') {
+            console.log("new value is:" + newValue);
             const parts = newValue.split('/');
             if (parts.length !== 3) {
                 cell.text(originalValue);
