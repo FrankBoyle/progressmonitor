5d2bae9e98edb1e13c04c5fed8ebea4a1131f0f5
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-11 18:52:00 -0500

    Update home.php

--- testing/home.php
+++ testing/home.php
@@ -198,7 +198,7 @@
             <h3 class="card-title"><strong>Choices</strong></h3>
           </div> <!-- /.card-body -->
           <div class="card-body">
-          <a href="https://fontawesome.com/">Font Awesome</a><br>
+          <a href="https://bfactor.org/testing/students.php">Students</a><br>
               <a href="https://useiconic.com/open/">Iconic Icons</a><br>
               <a href="https://ionicons.com/">Ion Icons</a><br>
             <div>
