85506922abff0b51403978860f86b83fa8aee28e
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-11 18:13:11 -0500

    Update home.php

--- testing/home.php
+++ testing/home.php
@@ -1,21 +1,9 @@
 <?php
-include './users/fetch_students.php';
-
-if (!isset($_SESSION['teacher_id'])) {
-    die("Teacher ID not set in session");
-}
-
-usort($students, function($a, $b) {
-  $aLastName = extractLastName($a['name']);
-  $bLastName = extractLastName($b['name']);
-  return strcmp($aLastName, $bLastName);
-});
-
-function extractLastName($fullName) {
-  $parts = explode(' ', $fullName);
-  return end($parts); // Assumes the last word is the last name
-}
-
+    session_start();
+    if(!isset($_SESSION["user"])) {
+        header("Location: login.php");
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
