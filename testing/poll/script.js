7f94640dbe38ce0bd849c7aa599c25e5a2c8bbc4
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:08:20 -0500

    Update script.js

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
                 itemsHtml += `
                     <div class="item" data-id="${item.id}">
                         <h3>${item.name}</h3>
-                        <p>Total Votes: ${item.total_votes}</p>
                         <label><input type="radio" name="first" value="${item.id}"> 1st Place</label>
                         <label><input type="radio" name="second" value="${item.id}"> 2nd Place</label>
                         <label><input type="radio" name="third" value="${item.id}"> 3rd Place</label>
@@ -21,24 +20,19 @@ $(document).ready(function() {
     $('#votingForm').on('submit', function(event) {
         event.preventDefault();
         var formData = $(this).serialize();
+        console.log("Form data being sent:", formData); // Debug log
         
         $.post('vote.php', formData, function(response) {
+            console.log("Response from server:", response); // Debug log
             alert("Votes submitted successfully!");
             displayItems(); // Refresh the items list
         });
     });
 
     displayItems(); // Initial display
-
-    // Event listener for second-place radio buttons
-    $('input[name="second"]').on('click', function() {
-        var secondValue = $(this).val();
-        var firstValue = $('input[name="first"]:checked').val();
-        if (secondValue && firstValue && secondValue === firstValue) {
-            alert("Please select a different item for 2nd place.");
-            $(this).prop('checked', false);
-        }
-    });
 });
+
+
+
 
 
