70eb1ae3df8733a0bc05c00bc7b3b212ee3b2ac8
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:25:15 -0500

    a

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -20,10 +20,7 @@ $(document).ready(function() {
                         <label for="third${item.id}"><i class="far fa-square"></i> 3rd Place</label>
                     </div>
                 </div>
-            `;         
-            
-            
-            
+            `;            
             });
             $('#itemsList').html(itemsHtml);
         });
