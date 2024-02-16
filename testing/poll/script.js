ff4c9417fc07645f153e53859d969c181731c84c
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:22:56 -0500

    a

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -8,19 +8,20 @@ $(document).ready(function() {
                 <div class="item" data-id="${item.id}">
                     <h3>${item.name}</h3>
                     <div class="toggle-button">
-                        <input type="radio" id="first${item.id}" name="first" value="${item.id}">
+                        <input type="radio" id="first${item.id}" name="first" value="${item.id}" hidden>
                         <label for="first${item.id}"><i class="far fa-square"></i> 1st Place</label>
                     </div>
                     <div class="toggle-button">
-                        <input type="radio" id="second${item.id}" name="second" value="${item.id}">
+                        <input type="radio" id="second${item.id}" name="second" value="${item.id}" hidden>
                         <label for="second${item.id}"><i class="far fa-square"></i> 2nd Place</label>
                     </div>
                     <div class="toggle-button">
-                        <input type="radio" id="third${item.id}" name="third" value="${item.id}">
+                        <input type="radio" id="third${item.id}" name="third" value="${item.id}" hidden>
                         <label for="third${item.id}"><i class="far fa-square"></i> 3rd Place</label>
                     </div>
                 </div>
             `;
+            
             
             
             });
