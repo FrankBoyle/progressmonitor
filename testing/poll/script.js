3625f4c81943aaadba2532e77a5c9af20d6b09c0
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 20:24:09 -0500

    Update script.js

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -1,6 +1,4 @@
-//script.js
 $(document).ready(function() {
-    // Function to fetch items and display them in the form
     function displayItems() {
         $.get('getItems.php', function(data) {
             var items = JSON.parse(data);
@@ -9,6 +7,7 @@ $(document).ready(function() {
                 itemsHtml += `
                     <div class="item" data-id="${item.id}">
                         <h3>${item.name}</h3>
+                        <p>Total Votes: ${item.total_votes}</p>
                         <label><input type="radio" name="first" value="${item.id}"> 1st Place</label>
                         <label><input type="radio" name="second" value="${item.id}"> 2nd Place</label>
                         <label><input type="radio" name="third" value="${item.id}"> 3rd Place</label>
@@ -19,23 +18,18 @@ $(document).ready(function() {
         });
     }
 
-    // Call displayItems when the page loads
-    displayItems();
-
-    // Handle form submission
     $('#votingForm').on('submit', function(event) {
         event.preventDefault();
         var formData = $(this).serialize();
         
         $.post('vote.php', formData, function(response) {
             alert("Votes submitted successfully!");
-            // Optionally, you can refresh the items list here if you want to show the updated vote counts
-            displayItems();
+            displayItems(); // Refresh the items list
         });
     });
+
+    displayItems(); // Initial display
 });
-
-
 
 
 
