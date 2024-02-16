0bd5f1458ac58d0a35582c6c9530de6fde770019
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-16 11:59:38 -0500

    Update SASpoll.php

--- testing/poll/SASpoll.php
+++ testing/poll/SASpoll.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Medal Voting System</title>
+    <title>SAS Poll Voting System</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
