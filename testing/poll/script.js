89c3552109393ca88748ddd207e62450554e938f
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:41:30 -0500

    a

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -6,23 +6,26 @@ $(document).ready(function() {
             items.forEach(function(item) {
                 itemsHtml += `
                 <div class="item" data-id="${item.id}">
-                    <h3>${item.name}</h3>
-                    <div class="toggle-button-container">
-                        <div class="toggle-button">
-                            <input type="radio" id="first${item.id}" name="first" value="${item.id}" hidden>
-                            <label for="first${item.id}" class="toggle-label">1st Place</label>
-                        </div>
-                        <div class="toggle-button">
-                            <input type="radio" id="second${item.id}" name="second" value="${item.id}" hidden>
-                            <label for="second${item.id}" class="toggle-label">2nd Place</label>
-                        </div>
-                        <div class="toggle-button">
-                            <input type="radio" id="third${item.id}" name="third" value="${item.id}" hidden>
-                            <label for="third${item.id}" class="toggle-label">3rd Place</label>
+                    <div class="item-content">
+                        <h3>${item.name}</h3>
+                        <div class="toggle-button-container">
+                            <div class="toggle-button">
+                                <input type="radio" id="first${item.id}" name="first" value="${item.id}" hidden>
+                                <label for="first${item.id}" class="toggle-label">1st Place</label>
+                            </div>
+                            <div class="toggle-button">
+                                <input type="radio" id="second${item.id}" name="second" value="${item.id}" hidden>
+                                <label for="second${item.id}" class="toggle-label">2nd Place</label>
+                            </div>
+                            <div class="toggle-button">
+                                <input type="radio" id="third${item.id}" name="third" value="${item.id}" hidden>
+                                <label for="third${item.id}" class="toggle-label">3rd Place</label>
+                            </div>
                         </div>
                     </div>
                 </div>
             `;
+            
             
             
             });
