7c23e6cfeb3bdc2eb3c312da1eda1ba9e0481c6f
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:13:42 -0500

    Update SASpoll.php

--- testing/poll/SASpoll.php
+++ testing/poll/SASpoll.php
@@ -6,12 +6,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<!-- Assume your items will be dynamically loaded here -->
-<form id="votingForm">
     <div id="itemsList"></div>
-    <input type="submit" value="Submit Votes">
-</form>
+
+    <!-- Assume your items will be dynamically loaded here -->
+    <form id="votingForm">
+        <div id="itemsList"></div>
+        <button class="vote-btn first" type="button" name="first">1st</button>
+        <button class="vote-btn second" type="button" name="second">2nd</button>
+        <button class="vote-btn third" type="button" name="third">3rd</button>
+        <input type="submit" value="Submit Votes">
+    </form>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="script.js"></script>
