4c47ac00d1acbef8c20847953756f2cd6615e81e
FrankBoyle <francisjboyleiii@gmail.com>
2024-02-15 21:26:42 -0500

    Update script.js

--- testing/poll/script.js
+++ testing/poll/script.js
@@ -5,24 +5,45 @@ $(document).ready(function() {
             var itemsHtml = '';
             items.forEach(function(item) {
                 itemsHtml += `
-                <div class="item" data-id="${item.id}">
-                    <h3>${item.name}</h3>
-                    <div class="toggle-button">
-                        <input type="radio" id="first${item.id}" name="first" value="${item.id}" hidden>
-                        <label for="first${item.id}"><i class="far fa-square"></i> 1st Place</label>
+                    <div class="item" data-id="${item.id}">
+                        <h3>${item.name}</h3>
+                        <div class="toggle-button">
+                            <input type="radio" id="first${item.id}" name="first" value="${item.id}" hidden>
+                            <label for="first${item.id}" class="toggle-label"><i class="far fa-square"></i> 1st Place</label>
+                        </div>
+                        <div class="toggle-button">
+                            <input type="radio" id="second${item.id}" name="second" value="${item.id}" hidden>
+                            <label for="second${item.id}" class="toggle-label"><i class="far fa-square"></i> 2nd Place</label>
+                        </div>
+                        <div class="toggle-button">
+                            <input type="radio" id="third${item.id}" name="third" value="${item.id}" hidden>
+                            <label for="third${item.id}" class="toggle-label"><i class="far fa-square"></i> 3rd Place</label>
+                        </div>
                     </div>
-                    <div class="toggle-button">
-                        <input type="radio" id="second${item.id}" name="second" value="${item.id}" hidden>
-                        <label for="second${item.id}"><i class="far fa-square"></i> 2nd Place</label>
-                    </div>
-                    <div class="toggle-button">
-                        <input type="radio" id="third${item.id}" name="third" value="${item.id}" hidden>
-                        <label for="third${item.id}"><i class="far fa-square"></i> 3rd Place</label>
-                    </div>
-                </div>
-            `;            
+                `;
             });
             $('#itemsList').html(itemsHtml);
+            
+            // Add event listeners to toggle buttons
+            $('.toggle-button').click(function() {
+                // Uncheck all other radio buttons in the same group
+                $(this).siblings().find('input[type="radio"]').prop('checked', false);
+                // Check the clicked radio button
+                $(this).find('input[type="radio"]').prop('checked', true);
+                // Update toggle button appearance
+                updateToggleButtons();
+            });
+        });
+    }
+
+    function updateToggleButtons() {
+        $('.toggle-button').each(function() {
+            var isChecked = $(this).find('input[type="radio"]').prop('checked');
+            if (isChecked) {
+                $(this).find('i').removeClass('far fa-square').addClass('fas fa-check-square');
+            } else {
+                $(this).find('i').removeClass('fas fa-check-square').addClass('far fa-square');
+            }
         });
     }
 
@@ -40,7 +61,6 @@ $(document).ready(function() {
 
     displayItems(); // Initial display
 });
-
 
 
 
