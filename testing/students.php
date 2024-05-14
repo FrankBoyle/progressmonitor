f9673cd248d1f4a47db3ec3d6a6408b28454cd82
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-05-14 11:17:18 -0400

    Update students.php

--- testing/students.php
+++ testing/students.php
@@ -166,7 +166,7 @@ function fetchStudentsByGroup($groupId) {
                     listItem.onclick = () => selectGroup(listItem);
 
                     const optionsBtn = document.createElement('button');
-                    optionsBtn.textContent = '+';
+                    optionsBtn.textContent = '⋮';
                     optionsBtn.classList.add('options-btn');
                     optionsBtn.onclick = (event) => showGroupOptions(event, group.group_id);
 
