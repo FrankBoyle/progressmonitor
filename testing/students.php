049be8f201d533865f4f208574b237744b9bfa3e
FrankBoyle <francisjboyleiii@gmail.com>
2024-05-14 20:26:37 -0400

    Update students.php

--- testing/students.php
+++ testing/students.php
@@ -94,13 +94,6 @@ function fetchStudentsByGroup($groupId) {
                 </div>
             </section>
 
-            <section class="box details">
-                <h3>Details</h3>
-                <ul>
-                    <li>Detail 1</li>
-                    <li>Detail 2</li>
-                </ul>
-            </section>
         </main>
     </div>
 
