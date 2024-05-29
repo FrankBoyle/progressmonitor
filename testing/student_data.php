5cbdabc09d8846c5689da31d400ca71a9cfa6930
FrankBoyle <francisjboyleiii@gmail.com>
2024-05-29 19:18:09 -0400

    Update student_data.php

--- testing/student_data.php
+++ testing/student_data.php
@@ -49,6 +49,7 @@ if (isset($_GET['metadata_id'])) {
     <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
     <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
     <link rel="stylesheet" href="styles.css">
