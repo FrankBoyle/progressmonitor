2f37e3f92b550bc2d21d1420de30921aa78be884
FrankBoyle <francisjboyleiii@gmail.com>
2024-05-29 18:09:34 -0400

    Update login.php

--- testing/login.php
+++ testing/login.php
@@ -16,7 +16,6 @@
 </head>
 
 <body>
-
     <div class="dashboard">
         <header class="dashboard-header">
             <div class="logo">Logo</div>
@@ -27,47 +26,21 @@
             </div>
         </header>
 
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in</p>
-    
-
-
-      <form method="post" action="./users/login_backend.php" name="login">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="email" placeholder="E-mail">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" value="login" name="login" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
+        
+        <main class="content">
+          <section class="box">
+            <h1 class="login-box-msg">Sign in</h1>
+              <form method="post" action="./users/login_backend.php" name="login">
+                <input type="text" class="form-control" name="email" placeholder="E-mail">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+                <span class="fas fa-lock"></span>
+                <input type="checkbox" id="remember">
+                <label for="remember">Remember Me</label>
+                <button type="submit" value="login" name="login" class="btn btn-primary btn-block">Sign In</button>
+              </form>
+          </section>
+        </main>
+    </div>
 
 
       <!--
