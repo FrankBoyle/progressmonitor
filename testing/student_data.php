77929ef1895595f4c7faf87f8db2c88eb8f5a5b2
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-05-29 14:07:43 -0400

    Update student_data.php

--- testing/student_data.php
+++ testing/student_data.php
@@ -47,6 +47,7 @@ if (isset($_GET['metadata_id'])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
