b71bb0fc5a493a3efe941c1baf22e2a99c4c7222
FrankBoyle <francisjboyleiii@gmail.com>
2024-05-29 18:15:30 -0400

    Update login.php

--- testing/login.php
+++ testing/login.php
@@ -14,7 +14,6 @@
     <style>
     </style>
 </head>
-
 <body>
     <div class="dashboard">
         <header class="dashboard-header">
@@ -26,61 +25,45 @@
             </div>
         </header>
 
-        
         <main class="content">
-          <section class="box">
-            <h1 class="login-box-msg">Sign in</h1>
-              <form method="post" action="./users/login_backend.php" name="login">
-                <input type="text" class="form-control" name="email" placeholder="E-mail">
-                <input type="password" class="form-control" name="password" placeholder="Password">
-                <span class="fas fa-lock"></span>
-                <input type="checkbox" id="remember">
-                <label for="remember">Remember Me</label>
-                <button type="submit" value="login" name="login" class="btn btn-primary btn-block">Sign In</button>
-              </form>
-            <p class="mb-1">
-              <?php
-                if (isset($_GET['reset']) && $_GET['reset'] == 1) {
-                  echo '<p class="info">If this email exists in our system, a reset link has been sent. Please check your inbox (and spam folder).</p>';
-                }
-              ?>
-              <form action="forgot_password.php" method="post">
-                <label for="email">Email:</label>
-                <input type="email" name="email" required>
-                <input type="submit" name="forgot_password" value="Request Password Reset">
-              </form>
-            </p>
-            <p class="mb-0">
-              <a href="register.php" class="text-center">Register a new membership</a>
-            </p>
-          </section>
+            <section class="box">
+                <h1 class="login-box-msg">Sign in</h1>
+                <form method="post" action="./users/login_backend.php" name="login">
+                    <div style="position: relative;">
+                        <input type="text" class="form-control" name="email" placeholder="E-mail" required>
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                    <div style="position: relative;">
+                        <input type="password" class="form-control" name="password" placeholder="Password" required>
+                        <span class="fas fa-lock"></span>
+                    </div>
+                    <div class="remember-me">
+                        <input type="checkbox" id="remember" name="remember">
+                        <label for="remember">Remember Me</label>
+                    </div>
+                    <button type="submit" value="login" name="login" class="btn btn-primary btn-block">Sign In</button>
+                </form>
+
+                <div class="forgot-password">
+                    <p class="mb-1">
+                        <?php
+                            if (isset($_GET['reset']) && $_GET['reset'] == 1) {
+                                echo '<p class="info">If this email exists in our system, a reset link has been sent. Please check your inbox (and spam folder).</p>';
+                            }
+                        ?>
+                        <form action="forgot_password.php" method="post">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" name="email" required>
+                            <input type="submit" class="btn btn-primary btn-block" name="forgot_password" value="Request Password Reset">
+                        </form>
+                    </p>
+                </div>
+
+                <p class="mb-0">
+                    <a href="register.php" class="text-center">Register a new membership</a>
+                </p>
+            </section>
         </main>
-      </div>
-
-
-      <!--
-      <div class="social-auth-links text-center mb-3">
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-        /.social-auth-links -->
-
-
-
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="https://bfactor.org">Bfactor.org</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
     </div>
-  </footer>
-
-
-
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-
 </body>
 </html>
