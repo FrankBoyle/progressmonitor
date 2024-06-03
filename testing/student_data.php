8517ce998d4000323277d0323a6cbdabb9b5c8b7
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-06-03 08:36:32 -0400

    Update student_data.php

--- testing/student_data.php
+++ testing/student_data.php
@@ -48,7 +48,6 @@ if (isset($_GET['metadata_id'])) {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
     <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
