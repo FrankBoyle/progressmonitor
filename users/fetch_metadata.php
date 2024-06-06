f3807147e0228413f9eaf59eceaa63790c68a555
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-06-06 13:11:44 -0400

    Update fetch_metadata.php

--- users/fetch_metadata.php
+++ users/fetch_metadata.php
@@ -3,10 +3,10 @@ include('db.php');
 
 // Assuming school_id comes from a session or a direct variable definition
 // For example, if it's in the session, you might get it like this:
-// $school_id = $_SESSION['school_id'];
+$school_id = $_SESSION['school_id'];
 
 // If it's being passed as a GET parameter, you might retrieve it like this:
-$school_id = $_GET['school_id']; // make sure to validate and sanitize this input
+//$school_id = $_GET['school_id']; // make sure to validate and sanitize this input
 
 try {
     // The SQL query now includes a WHERE clause to match the school_id
