196d394e5d384610c990b6d2a5c7be7a9fd0713f
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-06-24 13:00:35 -0400

    Update student_data2.php

--- testing/student_data2.php
+++ testing/student_data2.php
@@ -67,7 +67,7 @@
                     clipboardPasteParser:"range",
                     clipboardPasteAction:"range",
                     clipboardCopyConfig:{
-                        rowHeaders:true, //do not include row headers in clipboard output
+                        rowHeaders:false, //do not include row headers in clipboard output
                         columnHeaders:true, //do not include column headers in clipboard output
                     },
                     clipboardCopyStyled:false,
@@ -75,7 +75,6 @@
                     selectableRangeColumns:true,
                     selectableRangeRows:true,
                     selectableRangeClearCells:true,
-                    rowHeader: {resizable: false, frozen: true, width:40, hozAlign:"center", formatter: "rownum", field:"rownum", accessorClipboard:"rownum"},
                 });
 
                 // Add cellEdited event listener
