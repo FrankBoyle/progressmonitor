71ce37f18b183e17903ca38ca2fe69ce1bd458ad
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-24 21:03:58 -0400

    Update student_data2.php

--- testing/student_data2.php
+++ testing/student_data2.php
@@ -12,21 +12,20 @@
 <body>
 <div class="dashboard">
         <header class="dashboard-header">
-        <div class="logo">
-            <img src="bFactor_logo.png" alt="Logo">
-          </div>
+            <div class="logo">
+                <img src="bFactor_logo.png" alt="Logo">
+            </div>
             <div class="header-icons">
                 <a href="home.php" class="nav-link"><i class="nav-icon"></i>Home</a>
                 <a href="logout.php" class="nav-link"><i class="nav-icon"></i>Sign Out</a>
             </div>
         </header>
-        <div class="content">
-            <div id="performance-table">
-            </div>        
-        </div>
-    </div>
-
-
+            <main class="content">
+                <section class="box">
+                    <div id="performance-table"></div>  
+                </section>      
+            </main>
+</div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
