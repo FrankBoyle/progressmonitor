56de2b00879ffd18df97570affed2f100a9ad6fb
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-27 22:23:58 -0400

    a

--- testing/student_data.php
+++ testing/student_data.php
@@ -18,6 +18,15 @@ $schoolId = $_SESSION['school_id']; // Default to 1 if not set
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKFCCN4XXS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LKFCCN4XXS');
+</script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Layout</title>
