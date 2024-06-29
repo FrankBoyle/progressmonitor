d5431698e7ab21e93571c89feb472ded4ddca9aa
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-29 11:27:09 -0400

    Update charts.js

--- testing/charts.js
+++ testing/charts.js
@@ -1143,7 +1143,6 @@ function showPrintDialogModal() {
     const goalContainer = document.getElementById('goalSelectionContainer');
     goalContainer.innerHTML = ''; // Clear previous options
 
-    // Fetch goals and populate the container
     fetch(`./users/fetch_goals.php?student_id=${studentIdNew}&metadata_id=${metadataId}`)
         .then(response => response.json())
         .then(data => {
@@ -1156,7 +1155,7 @@ function showPrintDialogModal() {
                 const goalItem = document.createElement('div');
                 goalItem.classList.add('goal-item');
                 goalItem.setAttribute('data-goal-id', goal.goal_id);
-                goalItem.innerHTML = goal.goal_description; // Using plain HTML for goal description
+                goalItem.innerHTML = `<div class="quill-readonly">${goal.goal_description}</div>`;
                 goalItem.addEventListener('click', function() {
                     document.querySelectorAll('.goal-item').forEach(item => item.classList.remove('selected'));
                     goalItem.classList.add('selected');
