4a6a83bb26b94340ff824b65eaf7b6be002cbae2
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-29 16:30:10 -0400

    Update fetch_students.php

--- testing/users/fetch_students.php
+++ testing/users/fetch_students.php
@@ -1,23 +1,27 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-include('auth_session.php');
 include('db.php');
 
-$schoolId = $_SESSION['school_id'];
-$teacherId = $_SESSION['teacher_id'];
+$query = "SELECT student_id_new, first_name, last_name, group_id FROM Students_new LEFT JOIN StudentGroup ON Students_new.student_id_new = StudentGroup.student_id_new";
+$result = $connection->query($query);
 
-function fetchStudentsByTeacher($teacherId, $archived = false) {
-    global $connection;
-    $archivedValue = $archived ? 1 : 0;
-    $stmt = $connection->prepare("SELECT s.* FROM Students_new s INNER JOIN Teachers t ON s.school_id = t.school_id WHERE t.teacher_id = ? AND s.archived = ?");
-    $stmt->execute([$teacherId, $archivedValue]);
-    return $stmt->fetchAll(PDO::FETCH_ASSOC); // Fetch as associative array for JSON encoding
+$students = [];
+
+while ($row = $result->fetch_assoc()) {
+    if (!isset($students[$row['student_id_new']])) {
+        $students[$row['student_id_new']] = [
+            'student_id_new' => $row['student_id_new'],
+            'first_name' => $row['first_name'],
+            'last_name' => $row['last_name'],
+            'groups' => []
+        ];
+    }
+    if ($row['group_id'] !== null) {
+        $students[$row['student_id_new']]['groups'][] = $row['group_id'];
+    }
 }
 
-$allStudents = fetchStudentsByTeacher($teacherId, false);
+// Log the final students array
+error_log(print_r($students, true));
 
-echo json_encode($allStudents); // Output students as JSON
+echo json_encode(array_values($students));
 ?>
