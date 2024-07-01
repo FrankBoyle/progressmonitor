187abbebedfda0f1da9229c5833ed2f983e88480
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-30 22:17:20 -0400

    Update update_staff.php

--- testing/users/update_staff.php
+++ testing/users/update_staff.php
@@ -8,9 +8,13 @@ if (isset($data['teacher_id']) && isset($data['name']) && isset($data['subject_t
     $teacherId = $data['teacher_id'];
     $name = $data['name'];
     $subjectTaught = $data['subject_taught'];
-    $isAdmin = $data['is_admin'] === 'Yes' ? 1 : 0;
+    $isAdmin = $data['is_admin'] == '1' ? 1 : 0;
     
     try {
+        // Log the query and parameters for debugging
+        error_log("Updating teacher with ID: $teacherId");
+        error_log("Name: $name, Subject Taught: $subjectTaught, Is Admin: $isAdmin");
+        
         $query = $connection->prepare("UPDATE Teachers SET name = :name, subject_taught = :subject_taught, is_admin = :is_admin WHERE teacher_id = :teacher_id");
         $query->bindParam(':name', $name, PDO::PARAM_STR);
         $query->bindParam(':subject_taught', $subjectTaught, PDO::PARAM_STR);
