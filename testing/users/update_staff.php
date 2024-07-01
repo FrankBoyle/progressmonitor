f096fb386acc9ea05a1ae69065442fab4a87a901
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-30 22:26:08 -0400

    Update update_staff.php

--- testing/users/update_staff.php
+++ testing/users/update_staff.php
@@ -7,9 +7,9 @@ $data = json_decode(file_get_contents("php://input"), true);
 if (isset($data['teacher_id']) && isset($data['name']) && isset($data['is_admin'])) {
     $teacherId = $data['teacher_id'];
     $name = $data['name'];
-    $subjectTaught = isset($data['subject_taught']) && $data['subject_taught'] !== '' ? $data['subject_taught'] : null;
-    $isAdmin = $data['is_admin'] === 'Yes' ? 1 : 0;
-    
+    $subjectTaught = isset($data['subject_taught']) ? $data['subject_taught'] : null; // Allow subject_taught to be null
+    $isAdmin = $data['is_admin'];
+
     try {
         $query = $connection->prepare("UPDATE Teachers SET name = :name, subject_taught = :subject_taught, is_admin = :is_admin WHERE teacher_id = :teacher_id");
         $query->bindParam(':name', $name, PDO::PARAM_STR);
@@ -26,4 +26,5 @@ if (isset($data['teacher_id']) && isset($data['name']) && isset($data['is_admin'
     echo json_encode(['success' => false, 'message' => 'Invalid data']);
 }
 ?>
+
 
