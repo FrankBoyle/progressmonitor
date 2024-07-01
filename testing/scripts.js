f73ff20f3f20a77702250ed17a52a530103459b4
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-30 21:53:15 -0400

    Update scripts.js

--- testing/scripts.js
+++ testing/scripts.js
@@ -29,7 +29,7 @@ function loadUsers() {
             // Create approved users table
             const approvedTable = new Tabulator(approvedUsersTableContainer, {
                 data: approvedTableData,
-                layout: "fitDataFill",
+                layout: "fitDataStretch",
                 columns: [
                     { 
                         title: "Admin?", 
