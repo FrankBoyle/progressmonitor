396e734fc748d812cb8cf6b1b6de80b0796bf239
FrankBoyle <francisjboyleiii@gmail.com>
2024-06-30 21:18:30 -0400

    Update manage.php

--- testing/manage.php
+++ testing/manage.php
@@ -26,42 +26,41 @@ include('./users/auth_session.php');
 
 </head>
 <body>
+    <div class="dashboard">
+        <header class="dashboard-header">
+            <div class="logo">
+                <img src="bFactor_logo.png" alt="Logo">
+            </div>
+            <div class="header-icons">
+                <a href="students.php" class="nav-link">
+                    <i class="nav-icon"></i>
+                    <p>Home</p>
+                </a>
+                <a href="./users/logout.php" class="nav-link">
+                    <i class="nav-icon"></i>
+                    <p>Sign Out</p>
+                </a>
+            </div>
+        </header>
 
-<div class="dashboard">
-    <header class="dashboard-header">
-        <div class="logo">
-            <img src="bFactor_logo.png" alt="Logo">
-        </div>
-        <div class="header-icons">
-            <a href="students.php" class="nav-link">
-                <i class="nav-icon"></i>
-                <p>Home</p>
-            </a>
-            <a href="./users/logout.php" class="nav-link">
-                <i class="nav-icon"></i>
-                <p>Sign Out</p>
-            </a>
-        </div>
-    </header>
+        <main class="content-students">
+            <section class="box manage-section">
+                <h2>Manage Dashboard</h2>
 
-    <main class="content-students">
-        <section class="box manage-section">
-            <h2>Manage Dashboard</h2>
-            <div id="manage-content">
                 <h3>Manage Users</h3>
                 <div id="approved-users-table-container"></div>
+                
                 <h3>Users Waiting for Approval</h3>
-                <div id="unapproved-users-table-container"></div>
-            </div>
-        </section>
-    </main>
-</div>
+                <div id="waiting-approval-table-container"></div>
+            </section>
+        </main>
+    </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
-<script src="scripts.js"></script> <!-- Include the updated JavaScript for the manage page -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
+    <script src="https://unpkg.com/tabulator-tables@5.2.7/dist/js/tabulator.min.js"></script>
+    <script src="scripts.js"></script>
 </body>
 </html>
-
 
