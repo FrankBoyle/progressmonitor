71c10e488dc44285e5915303cd8910561d096ebb
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-06 19:12:28 -0300

    Update student_data.php

--- student_data.php
+++ student_data.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Assuming school_id is set in the session during login
-$schoolId = $_SESSION['school_id']; // Default to 1 if not set
+//$schoolId = $_SESSION['school_id']; // Default to 1 if not set
 //$teacher_id = $SESSION['teacher_id'];
 //$schoolId = $SESSION['school_id'];
 //$admin = $SESSION['is_admin'] == 1; // Assuming 'is_admin' is the column name
@@ -148,7 +148,7 @@ $schoolId = $_SESSION['school_id']; // Default to 1 if not set
     </div>
     <script src="charts.js"></script> <!-- Link to your external JS file that handles chart logic -->
     <script>
-        const schoolId = <?php echo json_encode($schoolId); ?>;
+        //const schoolId = <?php echo json_encode($schoolId); ?>;
     </script>
 </body>
 </html>
