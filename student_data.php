9f4c9710d3e73187650d483fd23a5c0ebd225607
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-06 21:57:35 -0300

    Update student_data.php

--- student_data.php
+++ student_data.php
@@ -1,9 +1,10 @@
 <?php
+include('auth_session.php');
+include('db.php');
 echo '<pre>';
 print_r($_SESSION);
 echo '</pre>';
-include('auth_session.php');
-include('db.php');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
