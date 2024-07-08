6ea671356729a130d4136c781a93839117424ef3
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-08 18:40:26 -0400

    Update charts.js

--- charts.js
+++ charts.js
@@ -505,6 +505,19 @@ function getLineChartOptions(dates, seriesData) {
             toolbar: {
                 show: true
             },
+            animations: {
+                enabled: true,
+                easing: 'easeinout',
+                speed: 800,
+                animateGradually: {
+                    enabled: true,
+                    delay: 150
+                },
+                dynamicAnimation: {
+                    enabled: true,
+                    speed: 350
+                }
+            },
             dropShadow: {
                 enabled: true,
                 top: 1,
