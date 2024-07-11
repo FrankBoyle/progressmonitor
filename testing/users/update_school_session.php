8c0a08f7bedc81d69fa29bd31d7ddd46a08c7e44
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-11 18:33:12 -0400

    Update update_school_session.php

--- testing/users/update_school_session.php
+++ testing/users/update_school_session.php
@@ -9,6 +9,7 @@ header('Content-Type: application/json');
 if (isset($_POST['school_id'])) {
     $schoolId = $_POST['school_id'];
     $_SESSION['school_id'] = $schoolId;
+    error_log("Session school_id updated to: " . $schoolId); // Debugging statement
     echo json_encode(['success' => true]);
 } else {
     echo json_encode(['success' => false, 'message' => 'School ID not provided']);
