7ef500008813462d16315b9c99538cb1089ef19c
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-19 19:09:00 -0400

    Update students.php

--- testing/students.php
+++ testing/students.php
@@ -35,6 +35,8 @@ $schools = $query->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/balzss/luxbar/ae5835e2/build/luxbar.min.css">
+
 
 </head>
 <body>
