8dd5d2741587b0b76f22525c64fe66af72c2134f
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 18:23:52 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -56,6 +56,7 @@
                     <h3 class="text-uppercase">Try It Now</h3>
                     <p class="lead">Create My Free Account in Seconds and Start Graphing Effortlessly</p>
                     <a class="btn btn-secondary btn-lg" href="register.php">Create My Free Account</a>
+                    <p>We will never share your information with anyone.</p>
                 </div>
             </div>
         </header>
