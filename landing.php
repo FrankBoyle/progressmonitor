22ce8b47a5d013a80bd4dfe7369ab5cbab1d7be8
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 18:08:25 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -48,7 +48,7 @@
                 <!-- Masthead Avatar Image-->
                 <img class="masthead-avatar mb-5" src="IEPreport_logo.jpg" alt="IEP Report Logo" />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Progress monitoring for IEPs has never been this easy!</h1>
+                <h4 class="masthead-heading text-uppercase mb-0">Progress monitoring for IEPs has never been this easy!</h4>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
