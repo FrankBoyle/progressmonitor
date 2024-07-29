c18abbc49d6f2e993ae84cafd0a19d81a0fde159
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 17:06:22 -0400

    a

--- landing.php
+++ landing.php
@@ -60,7 +60,9 @@
                 <!-- Icon Divider-->
                 <div class="divider-custom">
                     <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-icon">
+                        <img src="graduation_cap.png" alt="Graduation Cap Icon">
+                    </div>  
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Portfolio Grid Items-->
