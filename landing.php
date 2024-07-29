6b24a9645f5f5f68e63311cbef64eb78264187f1
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 13:35:31 -0400

    a

---
+++ landing.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">    <title>IEP Report</title>
+    <title>IEP Report</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="nav">
+            <a href="iepreport.com" target="_blank">IEP Report</a>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <button class="btn">Sign Up</button>
+                <button class="btn">Login</button>
+            </ul>
+            <i class="fas fa-bars" id="menu-btn_open"></i>
+            <i class="fas fa-times" id="menu-btn_close"></i>
+        </nav>
+    </div>
+
+
+</body>
+</html>
