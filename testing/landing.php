46f6761b0aa2e42aa322cc8a8f4b653f1db3dd32
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 14:41:52 -0400

    a

---
+++ testing/landing.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="landing_style.css">
+    <title>IEP Report</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="nav">
+            <a href="iepreport.com" target="_blank">IEP Report</a>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <button class="btn">Sign Up</button>
+                <button class="btn">Login</button>
+            </ul>
+            <i class="fa fa-bars" id="menu-btn_open"></i>
+            <i class="fa fa-times" id="menu-btn_close"></i>
+        </nav>
+
+
+        <header>
+            <h2>Hi,</h2>
+            <h2>I'm Norbert!</h2>
+            <h4>Welcome to my webpage</h4>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+            neque pariatur officia repellat necessitatibus, animi itaque
+            laboriosam iste, laborum tempora quibusdam voluptatem. Consequuntur
+            ipsam similique, id aperiam numquam aspernatur magni.
+            </p>
+            <button class="btn">Read more...</button>
+        </header>
+    </div>
+
+
+</body>
+</html>
