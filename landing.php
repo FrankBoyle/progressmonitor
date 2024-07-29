a5e2f3042de2546ff29406a6577e598adf63e3b4
FrankBoyle <francisjboyleiii@gmail.com>
2024-07-29 17:53:11 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -35,8 +35,8 @@
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
                     </ul>
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="login.php">Sign-In</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="register.php">Sign-Up</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="login.php">Sign-In</a></li>
                     </ul>
                 </div>
             </div>
